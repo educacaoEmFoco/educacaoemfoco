b0b60af42e74dd8c2b2da19315224699ac222daf
Ludimila da Bela Cruz <lbelacruz@gmail.com>
2013-09-25 11:06:39 -0300

    Criacao da tela lista estado

--- EEF/src/com/mdsgpp/eef/TelaListaEstado.java
+++ EEF/src/com/mdsgpp/eef/TelaListaEstado.java
@@ -1,8 +1,13 @@
 package com.mdsgpp.eef;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class TelaListaEstado extends Activity {
 
@@ -10,6 +15,19 @@ public class TelaListaEstado extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela_lista_estado);
+		
+		ListView listView = (ListView) findViewById(R.id.listview_tela_estados);
+		ArrayList<String> lista = new ArrayList<String>();
+		lista.add("Acre");
+		lista.add("Bahia");
+		lista.add("Ceara");
+		lista.add("Alagoas");
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String> (this,  R.layout.listview_item,lista);
+		listView.setAdapter(adapter);
+		
+		
+		
 	}
 
 	@Override
@@ -19,4 +37,5 @@ public class TelaListaEstado extends Activity {
 		return true;
 	}
 
+	
 }
