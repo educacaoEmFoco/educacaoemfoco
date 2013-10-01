4d86c020d44c15ad794d5e15554817921401f73e
Gabriel Silva <gabriel.ssilva@outlook.com>
2013-10-01 20:32:14 -0300

    Adicionada relacao entre a classe de parse com a classe de estado

---
+++ EEF/src/com/mdsgpp/eef/modelo/Estado.java
@@ -0,0 +1,127 @@
+package com.mdsgpp.eef.modelo;
+
+public class Estado {
+
+	private String nome;
+	private String sigla; 
+	private Censo censos[];//vetores, pois, será disponibilizado os valores referentes desde 2010
+	private double participacaoPercentualPIB[];
+	private int populacao[];
+	private int numeroDeProjetosCienciaTecnologia[];
+	private int valorInvestidoCienciaTecnologia[];
+	private Ideb idebs[];
+	private MediaNotasPorTurma mpt[];
+	private MediaHorasAulaDiaria mhad[];
+	private ProjetosContratados projetos[];
+	private TaxaDistorcaoIdadeSerie tdis[];
+	
+	public Estado(){
+	}
+	
+	public Estado(Censo censos[], double participacaoPercentualPIB[], int populacao[]){
+		this.censos = censos;
+		this.participacaoPercentualPIB = participacaoPercentualPIB;
+		this.populacao = populacao;
+	}
+
+	public Censo[] getCensos() {
+		return censos;
+	}
+
+	public void setCensos(Censo[] censos) {
+		this.censos = censos;
+	}
+
+	public double[] getParticipacaoPercentualPIB() {
+		return participacaoPercentualPIB;
+	}
+
+	public void setParticipacaoPercentualPIB(double[] participacaoPercentualPIB) {
+		this.participacaoPercentualPIB = participacaoPercentualPIB;
+	}
+
+	public int[] getPopulacao() {
+		return populacao;
+	}
+
+	public void setPopulacao(int[] populacao) {
+		this.populacao = populacao;
+	}
+
+	public int[] getNumeroDeProjetosCienciaTecnologia() {
+		return numeroDeProjetosCienciaTecnologia;
+	}
+
+	public void setNumeroDeProjetosCienciaTecnologia(
+			int[] numeroDeProjetosCienciaTecnologia) {
+		this.numeroDeProjetosCienciaTecnologia = numeroDeProjetosCienciaTecnologia;
+	}
+
+	public int[] getValorInvestidoCienciaTecnologia() {
+		return valorInvestidoCienciaTecnologia;
+	}
+
+	public void setValorInvestidoCienciaTecnologia(
+			int[] valorInvestidoCienciaTecnologia) {
+		this.valorInvestidoCienciaTecnologia = valorInvestidoCienciaTecnologia;
+	}
+
+	public Ideb[] getIdebs() {
+		return idebs;
+	}
+
+	public void setIdebs(Ideb[] idebs) {
+		this.idebs = idebs;
+	}
+
+	public MediaNotasPorTurma[] getMpt() {
+		return mpt;
+	}
+
+	public void setMpt(MediaNotasPorTurma[] mpt) {
+		this.mpt = mpt;
+	}
+
+	public MediaHorasAulaDiaria[] getMhad() {
+		return mhad;
+	}
+
+	public void setMhad(MediaHorasAulaDiaria[] mhad) {
+		this.mhad = mhad;
+	}
+
+	public ProjetosContratados[] getProjetos() {
+		return projetos;
+	}
+
+	public void setProjetos(ProjetosContratados[] projetos) {
+		this.projetos = projetos;
+	}
+
+	public TaxaDistorcaoIdadeSerie[] getTdis() {
+		return tdis;
+	}
+
+	public void setTdis(TaxaDistorcaoIdadeSerie[] tdis) {
+		this.tdis = tdis;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	
+	
+	
+}
---
+++ EEF/src/com/mdsgpp/eef/modelo/TaxaDeRendimento.java
@@ -0,0 +1,9 @@
+package com.mdsgpp.eef.modelo;
+
+public class TaxaDeRendimento {
+
+	public TaxaDeRendimento() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
