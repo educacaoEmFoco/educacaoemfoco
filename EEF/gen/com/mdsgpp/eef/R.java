b1a9271d08afe0d73c9feabf306ca2da1ad486b0
Vanusa <vanusa_marcelino@yahoo.com.br>
2013-10-06 22:22:15 -0300

    Alterando a cor do texto

--- EEF/gen/com/mdsgpp/eef/R.java
+++ EEF/gen/com/mdsgpp/eef/R.java
@@ -93,16 +93,10 @@ public final class R {
         public static final int title_activity_tela_lista_estado=0x7f050005;
     }
     public static final class style {
+        /**  Application theme. 
+         */
+        public static final int AppBaseColorTest=0x7f060001;
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -114,10 +108,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int AppBaseTheme=0x7f060002;
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f060000;
     }
 }
