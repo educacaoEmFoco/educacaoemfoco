88ce43fe3e7c8d4bfebbd97600269b250b5d1de6
Ludimila Bela <lbelacruz@gmail.com>
2013-10-07 07:46:15 -0300

    Inclusao de spinner na tela compara estado

--- EEF/src/com/mdsgpp/eef/TelaComparacao.java
+++ EEF/src/com/mdsgpp/eef/TelaComparacao.java
@@ -3,13 +3,26 @@ package com.mdsgpp.eef;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TelaComparacao extends Activity {
+	
+	Spinner spinnerestados; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela_comparacao);
+		
+		// Captura o spinner
+		spinnerestados = (Spinner) findViewById(R.id.spinner1);
+		
+		ArrayAdapter<CharSequence> estados = ArrayAdapter.createFromResource(this, R.array.estados, android.R.layout.simple_dropdown_item_1line);
+		estados.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		
+		// Define o adapter para o spinner
+		spinnerestados.setAdapter(estados);
 	}
 
 	@Override
