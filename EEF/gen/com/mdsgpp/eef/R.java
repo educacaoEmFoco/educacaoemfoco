1714d2696d2c1e22c17c90b52ba5ceba4107e1aa
Vanusa <vanusa_marcelino@yahoo.com.br>
2013-10-06 22:53:20 -0300

    Alteracao da cor da tela

--- EEF/gen/com/mdsgpp/eef/R.java
+++ EEF/gen/com/mdsgpp/eef/R.java
@@ -10,6 +10,9 @@ package com.mdsgpp.eef;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,48 +20,48 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080022;
-        public static final int botao_lista_estados=0x7f080001;
-        public static final int buttonComparacao=0x7f080000;
-        public static final int linearLayoutHorizontal=0x7f08001f;
-        public static final int linearlayout_estado=0x7f080004;
-        public static final int listview_tela_estados=0x7f08001e;
-        public static final int scrollView1=0x7f080003;
-        public static final int textView_ideb=0x7f08000e;
-        public static final int textView_ideb_ensinomedio=0x7f080010;
-        public static final int textView_ideb_fundamental=0x7f08000f;
-        public static final int textView_ideb_iniciais=0x7f080011;
-        public static final int textView_jovens_pesquisadores=0x7f080018;
-        public static final int textView_jovens_pesquisadores_quantidade=0x7f080019;
-        public static final int textView_jovens_pesquisadores_valor=0x7f08001a;
-        public static final int textView_nome_estado=0x7f080006;
-        public static final int textView_numero_projetos=0x7f08000b;
-        public static final int textView_numero_projetos_quantidade=0x7f08000c;
-        public static final int textView_participacao_estadual_pib=0x7f080009;
-        public static final int textView_participacao_pib_valor=0x7f08000a;
-        public static final int textView_populacao=0x7f080007;
-        public static final int textView_populacao_valor=0x7f080008;
-        public static final int textView_primeiros_projetos_quantidade=0x7f080013;
-        public static final int textView_programa_primeiros_projetos=0x7f080012;
-        public static final int textView_programa_primeiros_projetos_valor=0x7f080014;
-        public static final int textView_projetos_iniciacao=0x7f08001b;
-        public static final int textView_projetos_iniciacao_quantidade=0x7f08001c;
-        public static final int textView_projetos_iniciacao_valor=0x7f08001d;
-        public static final int textView_projetos_pesquisa=0x7f080015;
-        public static final int textView_projetos_pesquisa_quantidade=0x7f080016;
-        public static final int textView_sigla=0x7f080005;
-        public static final int textView_valor_investido_projetos=0x7f08000d;
-        public static final int textView_valores_projeto_pesquisa=0x7f080017;
-        public static final int text_view_comparacao=0x7f080002;
-        public static final int textview_lista_estados=0x7f080021;
-        public static final int textview_lista_siglas=0x7f080020;
+        public static final int action_settings=0x7f090022;
+        public static final int botao_lista_estados=0x7f090001;
+        public static final int buttonComparacao=0x7f090000;
+        public static final int linearLayoutHorizontal=0x7f09001f;
+        public static final int linearlayout_estado=0x7f090004;
+        public static final int listview_tela_estados=0x7f09001e;
+        public static final int scrollView1=0x7f090003;
+        public static final int textView_ideb=0x7f09000e;
+        public static final int textView_ideb_ensinomedio=0x7f090010;
+        public static final int textView_ideb_fundamental=0x7f09000f;
+        public static final int textView_ideb_iniciais=0x7f090011;
+        public static final int textView_jovens_pesquisadores=0x7f090018;
+        public static final int textView_jovens_pesquisadores_quantidade=0x7f090019;
+        public static final int textView_jovens_pesquisadores_valor=0x7f09001a;
+        public static final int textView_nome_estado=0x7f090006;
+        public static final int textView_numero_projetos=0x7f09000b;
+        public static final int textView_numero_projetos_quantidade=0x7f09000c;
+        public static final int textView_participacao_estadual_pib=0x7f090009;
+        public static final int textView_participacao_pib_valor=0x7f09000a;
+        public static final int textView_populacao=0x7f090007;
+        public static final int textView_populacao_valor=0x7f090008;
+        public static final int textView_primeiros_projetos_quantidade=0x7f090013;
+        public static final int textView_programa_primeiros_projetos=0x7f090012;
+        public static final int textView_programa_primeiros_projetos_valor=0x7f090014;
+        public static final int textView_projetos_iniciacao=0x7f09001b;
+        public static final int textView_projetos_iniciacao_quantidade=0x7f09001c;
+        public static final int textView_projetos_iniciacao_valor=0x7f09001d;
+        public static final int textView_projetos_pesquisa=0x7f090015;
+        public static final int textView_projetos_pesquisa_quantidade=0x7f090016;
+        public static final int textView_sigla=0x7f090005;
+        public static final int textView_valor_investido_projetos=0x7f09000d;
+        public static final int textView_valores_projeto_pesquisa=0x7f090017;
+        public static final int text_view_comparacao=0x7f090002;
+        public static final int textview_lista_estados=0x7f090021;
+        public static final int textview_lista_siglas=0x7f090020;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -68,35 +71,33 @@ public final class R {
         public static final int listview_item=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int tela_comparacao=0x7f070001;
-        public static final int tela_estado=0x7f070002;
-        public static final int tela_lista_estado=0x7f070003;
+        public static final int main=0x7f080000;
+        public static final int tela_comparacao=0x7f080001;
+        public static final int tela_estado=0x7f080002;
+        public static final int tela_lista_estado=0x7f080003;
     }
     public static final class string {
-        public static final int Programa_primeiros_projetos=0x7f05000d;
-        public static final int Projetos_pesquisa_CNPq=0x7f05000e;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int jovens_pesquisadores=0x7f05000f;
-        public static final int menu_botao_comparacao=0x7f050007;
-        public static final int menu_botao_lista_estados=0x7f050006;
-        public static final int numero_projetos=0x7f05000b;
-        public static final int participacao_IDEB=0x7f05000c;
-        public static final int participacao_PIB=0x7f05000a;
-        public static final int populacao=0x7f050009;
-        public static final int projeto_iniciacao=0x7f050010;
-        public static final int string_vazia=0x7f050008;
-        public static final int title_activity_tela_comparacao=0x7f050004;
-        public static final int title_activity_tela_estado=0x7f050003;
-        public static final int title_activity_tela_lista_estado=0x7f050005;
+        public static final int Programa_primeiros_projetos=0x7f06000d;
+        public static final int Projetos_pesquisa_CNPq=0x7f06000e;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int jovens_pesquisadores=0x7f06000f;
+        public static final int menu_botao_comparacao=0x7f060007;
+        public static final int menu_botao_lista_estados=0x7f060006;
+        public static final int numero_projetos=0x7f06000b;
+        public static final int participacao_IDEB=0x7f06000c;
+        public static final int participacao_PIB=0x7f06000a;
+        public static final int populacao=0x7f060009;
+        public static final int projeto_iniciacao=0x7f060010;
+        public static final int string_vazia=0x7f060008;
+        public static final int title_activity_tela_comparacao=0x7f060004;
+        public static final int title_activity_tela_estado=0x7f060003;
+        public static final int title_activity_tela_lista_estado=0x7f060005;
     }
     public static final class style {
         /**  Application theme. 
-         */
-        public static final int AppBaseColorTest=0x7f060001;
-        /** 
+
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -108,12 +109,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060002;
+        public static final int AppBaseTheme=0x7f070001;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
     
          */
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }
