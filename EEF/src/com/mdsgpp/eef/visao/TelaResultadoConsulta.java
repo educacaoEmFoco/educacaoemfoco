5ab5abf9106ebce5cfdc65f7017c204d5033bb23
Ludimila da Bela <lbelacruz@gmail.com>
2013-11-09 16:16:59 -0200

    Inclusao de campos no metodo escondeCamposDeTexto co-piloto: Marcos, piloto: Ludimila

--- EEF/src/com/mdsgpp/eef/visao/TelaResultadoConsulta.java
+++ EEF/src/com/mdsgpp/eef/visao/TelaResultadoConsulta.java
@@ -21,20 +21,28 @@ public class TelaResultadoConsulta extends Activity {
 	private TextView textViewNome1;
 	private TextView textViewPopulacao1;
 	private TextView textViewPopulacaoValor1;
+	private TextView textViewParticipacaoEstadualPib1;
 	private TextView textViewParticipacaoPib1;
+	private TextView textViewNumeroProjetos1;
 	private TextView textViewQuantidadeProjetosCienciaTecnologia1;
 	private TextView textViewValorProjetosCienciaTecnologia1;
+	private TextView textViewIdeb1;
 	private TextView textViewFundamentalIdeb1;
 	private TextView textViewEnsinoMedioIdeb1;
 	private TextView textViewIniciaisIdeb1;
+	private TextView textViewProgramaPrimeirosProjetos1;
 	private TextView textViewQuantidadePrimeirosProjetos1;
 	private TextView textViewValorPrimeirosProjetos1;
+	private TextView textViewProjetosPesquisa1;
 	private TextView textViewQuantidadePesquisa1;
+	private TextView textViewJovensPesquisadores1;
 	private TextView textViewQuantidadeJovensPesquisadores1;
 	private TextView textViewValoresPesquisa1;
 	private TextView textViewValorJovensPesquisadores1;
+	private TextView textViewProjetosIniciacao1;
 	private TextView textViewQuantidadeProjetosIniciacao1;
 	private TextView textViewValorProjetosIniciacao1;
+	
 
 	private TextView textViewSigla2;
 	private TextView textViewNome2;
@@ -188,20 +196,27 @@ public class TelaResultadoConsulta extends Activity {
 		textViewNome1 = (TextView) findViewById(R.id.textView_nome_estado1);		
 		textViewPopulacaoValor1 = (TextView) findViewById(R.id.textView_populacao_valor1);
 		textViewPopulacao1 = (TextView) findViewById(R.id.textView_populacao1);
+		textViewParticipacaoEstadualPib1 = (TextView) findViewById(R.id.textView_participacao_estadual_pib1);
 		textViewParticipacaoPib1 = (TextView) findViewById(R.id.textView_participacao_pib_valor1);
+		textViewNumeroProjetos1 =  (TextView) findViewById(R.id.textView_numero_projetos1);
 		textViewQuantidadeProjetosCienciaTecnologia1 = (TextView) findViewById(R.id.textView_numero_projetos_quantidade1);
 		textViewValorProjetosCienciaTecnologia1 = (TextView) findViewById(R.id.textView_valor_investido_projetos1);
+		textViewIdeb1 = (TextView) findViewById(R.id.textView_ideb1);
 		textViewFundamentalIdeb1 = (TextView) findViewById(R.id.textView_ideb_fundamental1);
 		textViewEnsinoMedioIdeb1 = (TextView) findViewById(R.id.textView_ideb_ensinomedio1);
 		textViewIniciaisIdeb1 = (TextView) findViewById(R.id.textView_ideb_iniciais1);
+		textViewProgramaPrimeirosProjetos1 = (TextView) findViewById(R.id.textView_programa_primeiros_projetos1);
 		textViewQuantidadePrimeirosProjetos1 = (TextView) findViewById(R.id.textView_primeiros_projetos_quantidade1);
 		textViewValorPrimeirosProjetos1 = (TextView) findViewById(R.id.textView_programa_primeiros_projetos_valor1);
+		textViewProjetosPesquisa1 = (TextView) findViewById(R.id.textView_projetos_pesquisa1);
 		textViewQuantidadePesquisa1 = (TextView) findViewById(R.id.textView_projetos_pesquisa_quantidade1);
 		textViewValoresPesquisa1 = (TextView) findViewById(R.id.textView_valores_projeto_pesquisa1);
 		textViewQuantidadeJovensPesquisadores1 = (TextView) findViewById(R.id.textView_jovens_pesquisadores_quantidade1);
 		textViewValorJovensPesquisadores1 = (TextView) findViewById(R.id.textView_jovens_pesquisadores_valor1);
+		textViewProjetosIniciacao1 = (TextView) findViewById(R.id.textView_projetos_iniciacao1);
 		textViewQuantidadeProjetosIniciacao1 = (TextView) findViewById(R.id.textView_projetos_iniciacao_quantidade1);
 		textViewValorProjetosIniciacao1 = (TextView) findViewById(R.id.textView_projetos_iniciacao_valor1);
+		
 		
 		textViewSigla2 = (TextView) findViewById(R.id.textView_sigla2);
 		textViewNome2 = (TextView) findViewById(R.id.textView_nome_estado2);
@@ -216,6 +231,7 @@ public class TelaResultadoConsulta extends Activity {
 		textViewValorPrimeirosProjetos2 = (TextView) findViewById(R.id.textView_programa_primeiros_projetos_valor2);
 		textViewQuantidadePesquisa2 = (TextView) findViewById(R.id.textView_projetos_pesquisa_quantidade2);
 		textViewValoresPesquisa2 = (TextView) findViewById(R.id.textView_valores_projeto_pesquisa2);
+		textViewJovensPesquisadores1 = (TextView) findViewById(R.id.textView_jovens_pesquisadores1);
 		textViewQuantidadeJovensPesquisadores2 = (TextView) findViewById(R.id.textView_jovens_pesquisadores_quantidade2);
 		textViewValorJovensPesquisadores2 = (TextView) findViewById(R.id.textView_jovens_pesquisadores_valor2);
 		textViewQuantidadeProjetosIniciacao2 = (TextView) findViewById(R.id.textView_projetos_iniciacao_quantidade2);
@@ -225,9 +241,74 @@ public class TelaResultadoConsulta extends Activity {
 	
 	public void escondeCamposDeTexto() {
 		int vPopulacao = (bPopulacao) ? View.VISIBLE : View.GONE;
+		
 			textViewPopulacao1.setVisibility(vPopulacao);
 			textViewPopulacaoValor1.setVisibility(vPopulacao);
 			textViewPopulacaoValor2.setVisibility(vPopulacao);
+			
+		int vIdeb = (bIdeb) ? View.VISIBLE : View.GONE;
+		
+			textViewIdeb1.setVisibility(vIdeb);
+			textViewFundamentalIdeb1.setVisibility(vIdeb);
+			textViewEnsinoMedioIdeb1.setVisibility(vIdeb);
+			textViewIniciaisIdeb1.setVisibility(vIdeb);
+			textViewFundamentalIdeb2.setVisibility(vIdeb);
+			textViewEnsinoMedioIdeb2.setVisibility(vIdeb);
+			textViewIniciaisIdeb2.setVisibility(vIdeb);
+			
+		 int vPib = (bPib) ? View.VISIBLE : View.GONE;	
+		
+		    textViewParticipacaoEstadualPib1.setVisibility(vPib);
+		    textViewParticipacaoPib1.setVisibility(vPib);
+			textViewParticipacaoPib2.setVisibility(vPib);
+			
+		int vPrimeirosProjetos = (bPrimeirosProjetos) ? View.VISIBLE : View.GONE;
+			
+			textViewProgramaPrimeirosProjetos1.setVisibility(vPrimeirosProjetos);
+			textViewQuantidadePrimeirosProjetos1.setVisibility(vPrimeirosProjetos);
+			textViewValorPrimeirosProjetos1.setVisibility(vPrimeirosProjetos);
+			textViewQuantidadePrimeirosProjetos2.setVisibility(vPrimeirosProjetos);
+			textViewValorPrimeirosProjetos2.setVisibility(vPrimeirosProjetos);
+			
+		int vProjetosCnpq = (bProjetosCnpq) ? View.VISIBLE : View.GONE;	
+		
+			textViewProjetosPesquisa1.setVisibility(vProjetosCnpq);
+			textViewQuantidadePesquisa1.setVisibility(vProjetosCnpq);
+			textViewValoresPesquisa1.setVisibility(vProjetosCnpq);
+			textViewQuantidadePesquisa2.setVisibility(vProjetosCnpq);
+			textViewValoresPesquisa2.setVisibility(vProjetosCnpq);
+				
+			
+		int vProjetosJovens = (bProjetosJovens) ? View.VISIBLE : View.GONE;	
+			
+			
+			textViewJovensPesquisadores1.setVisibility(vProjetosJovens);
+			textViewQuantidadeJovensPesquisadores1.setVisibility(vProjetosJovens); 
+			textViewValorJovensPesquisadores1.setVisibility(vProjetosJovens);
+			textViewQuantidadeJovensPesquisadores2.setVisibility(vProjetosJovens); 
+			textViewValorJovensPesquisadores2.setVisibility(vProjetosJovens);  
+				
+		int vProjetosIniciacao = (bProjetosIniciacao) ? View.VISIBLE : View.GONE;	
+		
+		
+			textViewProjetosIniciacao1.setVisibility(vProjetosIniciacao); 
+			textViewQuantidadeProjetosIniciacao1.setVisibility(vProjetosIniciacao); 
+			textViewValorProjetosIniciacao1.setVisibility(vProjetosIniciacao );
+			textViewQuantidadeProjetosIniciacao2.setVisibility(vProjetosIniciacao); 
+			textViewValorProjetosIniciacao2.setVisibility(vProjetosIniciacao);
+		
+			
+		int vProjetosDifusao = (bProjetosDifusao) ? View.VISIBLE : View.GONE;	
+		
+			textViewNumeroProjetos1.setVisibility(vProjetosDifusao);
+			textViewQuantidadeProjetosCienciaTecnologia1.setVisibility(vProjetosDifusao);
+			textViewValorProjetosCienciaTecnologia1.setVisibility(vProjetosDifusao);	
+			textViewQuantidadeProjetosCienciaTecnologia2.setVisibility(vProjetosDifusao);
+			textViewValorProjetosCienciaTecnologia2.setVisibility(vProjetosDifusao);
+						
+						
+					
+			
 	}
 }
 
