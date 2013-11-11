cdddd54aa9689099510cb53d603d0fbdc0c36eec
Ludimila Bela <lbelacruz@gmail.com>
2013-11-11 16:07:23 -0200

    Adicao da classe do parse FeedParser

---
+++ EEF/src/com/mdsgpp/eef/parse/FeedParser.java
@@ -0,0 +1,108 @@
+package com.mdsgpp.eef.parse;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import com.mdsgpp.eef.controle.FeedParseControle;
+import com.mdsgpp.eef.modelo.Feed;
+
+import android.sax.Element;
+import android.sax.EndElementListener;
+import android.sax.EndTextElementListener;
+import android.sax.RootElement;
+import android.sax.StartElementListener;
+import android.util.Xml;
+
+public class FeedParser extends DefaultHandler {
+	
+	public Feed parse(InputStream is) {
+
+		// Declaration of each element tag that we want to treat
+        RootElement root = new RootElement("rss");
+        Element chanElement = root.getChild("channel");
+
+        Element chanItem = chanElement.getChild("item");
+        Element itemTitle = chanItem.getChild("title");
+        Element itemDescription = chanItem.getChild("description");
+        Element itemCategory = chanItem.getChild("category");
+        Element itemPubDate = chanItem.getChild("pubDate");
+        Element itemLink = chanItem.getChild("link");
+
+        /* To every tag, we define the desired action to be performed, for example:
+           When the 'channel' element is opened */
+        chanElement.setStartElementListener(new StartElementListener() {
+            public void start(Attributes attributes) {
+                FeedParseControle.getInstance().criaNovoFeed();
+            }
+        });
+
+        // when the 'item' element is opened
+        chanItem.setStartElementListener(new StartElementListener() {
+            public void start(Attributes attributes) {
+                FeedParseControle.getInstance().criaNovoItem();
+            }
+        });
+
+        // When the 'item' element is closed
+        chanItem.setEndElementListener(new EndElementListener() {
+            public void end() {
+                FeedParseControle.getInstance().adicionaItem();
+            }
+        });
+
+        // When the 'title' element (inside 'item') is closed, we can get the text
+        itemTitle.setEndTextElementListener(new EndTextElementListener() {
+            public void end(String body) {
+            	FeedParseControle.getInstance().getItem().setTitle(body);
+            }
+        });
+        
+        // When the 'description' element (inside 'item') is closed, we can get the text
+        itemDescription.setEndTextElementListener(new EndTextElementListener() {
+			@Override
+			public void end(String body) {
+				FeedParseControle.getInstance().getItem().setDescription(body);
+			}
+		});
+        
+        // When the 'category' element (inside 'item') is closed, we can get the text
+        itemCategory.setEndTextElementListener(new EndTextElementListener() {
+			@Override
+			public void end(String body) {
+				FeedParseControle.getInstance().getItem().setCategory(body);
+			}
+		});
+        
+        // When the 'pubDate' element (inside 'item') is closed, we can get the text
+        itemPubDate.setEndTextElementListener(new EndTextElementListener() {
+			@Override
+			public void end(String body) {
+				FeedParseControle.getInstance().getItem().setDate(body);
+			}
+		});
+        
+        // When the 'link' element (inside 'item') is closed, we can get the text
+        itemLink.setEndTextElementListener(new EndTextElementListener() {
+			@Override
+			public void end(String body) {
+				FeedParseControle.getInstance().getItem().setLink(body);
+			}
+		});
+
+        // Finally, we parse the xml file (through the InputStream) and return it
+        try {
+            Xml.parse(is, Xml.Encoding.UTF_8, root.getContentHandler());
+            return FeedParseControle.getInstance().getFeed();
+        } catch (SAXException e) {
+            // XML conversion error
+        } catch (IOException e) {
+            // file opening error
+        }
+
+        return null;
+    }
+}
