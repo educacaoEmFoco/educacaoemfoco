382b583d3337511911056f89ce86b1a7c1b159dc
Ludimila Bela <lbelacruz@gmail.com>
2013-11-11 16:03:27 -0200

    Adicao da classe de persistencia FeedPersistencia

---
+++ EEF/src/com/mdsgpp/eef/parse/FeedPersistencia.java
@@ -0,0 +1,52 @@
+package com.mdsgpp.eef.parse;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import com.mdsgpp.eef.modelo.Feed;
+
+import android.content.Context;
+
+public class FeedPersistencia {
+
+	private static final String FILENAME = "feed.txt";
+	private static FeedPersistencia instancia;
+	private Context context;
+	
+	public FeedPersistencia(Context context) {
+		this.context = context;
+	}
+
+	public static FeedPersistencia getInstance(Context context) {
+		if (instancia == null) {
+			instancia = new FeedPersistencia(context);
+		}
+		return instancia;
+	}
+	
+	public void writeFeedFile(Feed feed) throws IOException {
+		FileOutputStream fileout = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+		ObjectOutputStream out = new ObjectOutputStream(fileout);
+
+		out.writeObject(feed);
+		out.close();
+	}
+
+	public Feed readFeedFile() throws IOException, ClassNotFoundException {
+
+		Feed feed;
+
+
+		FileInputStream filein = context.openFileInput(FILENAME);
+		ObjectInputStream in = new ObjectInputStream(filein);
+
+		feed = (Feed) in.readObject();
+		in.close();
+
+		return feed;
+	}
+
+}
