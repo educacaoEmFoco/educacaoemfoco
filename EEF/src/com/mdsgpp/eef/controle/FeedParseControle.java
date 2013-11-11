68a1fd522c1ac6d78e566b5857e747fc958dc1a3
Bruna Moreira <brunanayramlima@gmail.com>
2013-11-11 16:36:13 -0200

    Adicionando classe FeedParseControle. Piloto: Marcos, Co-Piloto: Bruna

--- EEF/src/com/mdsgpp/eef/controle/FeedParseControle.java
+++ EEF/src/com/mdsgpp/eef/controle/FeedParseControle.java
@@ -1,10 +1,43 @@
-package com.mdsgpp.eef.controle;
+package com.rssfeed.control;
+
+import com.rssfeed.model.Feed;
+import com.rssfeed.model.Noticias;
 
 public class FeedParseControle {
 
-	public static Object getInstance() {
-		// TODO Auto-generated method stub
-		return null;
+	private Feed feed;
+	private Noticias item;
+	private static FeedParseControle instancia;
+	
+	public FeedParseControle() {
+		this.feed = new Feed();
+	}
+	
+	public static FeedParseControle getInstance() {
+		if (instancia == null) {
+			instancia = new FeedParseControle();
+		}
+		return instancia;
 	}
 
+	public Feed getFeed() {
+		return this.feed;
+	}
+	
+	public void criaNovoFeed() {
+		this.feed.reset();
+	}
+	
+	public void criaNovoItem() {
+		this.item = new Noticias(this.feed);
+	}
+	
+	public void adicionaItem() {
+		this.feed.addItem(this.item);
+	}
+	
+	public Noticias getItem() {
+		return this.item;
+	}
+	
 }
