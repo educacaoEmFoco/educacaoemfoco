d6a4e49e57bb326e31eeb89a992cd34c6b1b940a
Vanusa <vanusa_marcelino@yahoo.com.br>
2013-11-12 11:36:43 -0200

    Teste feed  piloto:Vanusa Co-piloto:Lucas

---
+++ EEF/src/com/mdsgpp/eef/testes/FeedTeste.java
@@ -0,0 +1,66 @@
+package com.mdsgpp.eef.testes;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import android.test.AndroidTestCase;
+
+import com.mdsgpp.eef.modelo.Feed;
+import com.mdsgpp.eef.modelo.Noticias;
+
+public class FeedTeste extends AndroidTestCase {
+
+	private ArrayList<Noticias> itens;
+	private Feed feed;
+	private Noticias noticias;
+	
+	@Before
+	public void setUp(){
+		
+		itens = new ArrayList<Noticias>();
+		feed = new Feed();
+		noticias = new Noticias(feed);
+		
+		itens.add(noticias);
+		feed.setItems(itens);
+	}
+	
+	@Test
+	public void testSetArrayListDeNoticias() {
+
+		assertEquals(itens,feed.getItems());
+		
+	}
+	
+	@Test
+	public void testGetSizeDoArrayDeNoticias(){
+		
+		assertEquals(1,feed.getItemsSize());
+	}
+	
+	@Test
+	public void testResetDoArrayListDeNoticias(){
+		feed.reset();
+		
+		assertEquals(0,feed.getItemsSize());
+	}
+	
+	@Test
+	public void testGetItemDoArrayDeNoticias(){
+		assertEquals(noticias,feed.getItem(0));
+	}
+	
+	@Test
+	public void testAdicionarItensNoArrayDeNoticias(){
+		feed.addItem(noticias);
+		assertEquals(2,feed.getItemsSize());
+	}
+	
+	
+	
+
+}
