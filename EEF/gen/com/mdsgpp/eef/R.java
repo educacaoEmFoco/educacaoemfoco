811b8a4a31cbf37544b39101ecffa7e87e4f271e
Marcos Dourado <marcos_bdrlr@hotmail.com>
2013-11-19 13:03:57 -0200

    termino da implementação da TelaEscolheIndicativosGrafico

--- EEF/gen/com/mdsgpp/eef/R.java
+++ EEF/gen/com/mdsgpp/eef/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int blue=0x7f040001;
-        public static final int white=0x7f040000;
+        public static final int blue=0x7f070001;
+        public static final int white=0x7f070000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -20,9 +20,14 @@ public final class R {
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
+ Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int acre=0x7f020000;
@@ -47,15 +52,16 @@ public final class R {
         public static final int parana=0x7f020013;
         public static final int pernambuco=0x7f020014;
         public static final int piaui=0x7f020015;
-        public static final int riodejaneiro=0x7f020016;
-        public static final int riograndedonorte=0x7f020017;
-        public static final int riograndedosul=0x7f020018;
-        public static final int rondonia=0x7f020019;
-        public static final int roraima=0x7f02001a;
-        public static final int santacatarina=0x7f02001b;
-        public static final int saopaulo=0x7f02001c;
-        public static final int sergipe=0x7f02001d;
-        public static final int tocantins=0x7f02001e;
+        public static final int popup_black=0x7f020016;
+        public static final int riodejaneiro=0x7f020017;
+        public static final int riograndedonorte=0x7f020018;
+        public static final int riograndedosul=0x7f020019;
+        public static final int rondonia=0x7f02001a;
+        public static final int roraima=0x7f02001b;
+        public static final int santacatarina=0x7f02001c;
+        public static final int saopaulo=0x7f02001d;
+        public static final int sergipe=0x7f02001e;
+        public static final int tocantins=0x7f02001f;
     }
     public static final class id {
         public static final int action_settings=0x7f09008b;
@@ -236,90 +242,110 @@ public final class R {
         public static final int tela_sobre=0x7f08000f;
     }
     public static final class string {
-        public static final int Programa_primeiros_projetos=0x7f060020;
-        public static final int Projetos_pesquisa_CNPq=0x7f060021;
-        public static final int action_settings=0x7f060001;
+        public static final int Programa_primeiros_projetos=0x7f050020;
+        public static final int Projetos_pesquisa_CNPq=0x7f050021;
+        public static final int action_settings=0x7f050001;
         /**  Strings da tela inicial 
          */
-        public static final int app_name=0x7f060000;
-        public static final int botao_gera_grafico=0x7f060007;
-        public static final int botao_sobre=0x7f060039;
+        public static final int app_name=0x7f050000;
+        public static final int botao_gera_grafico=0x7f050007;
+        public static final int botao_sobre=0x7f050039;
         /**  Strings da tela indicativos consultados 
          */
-        public static final int botao_tela_indicativos=0x7f06000c;
-        public static final int bt_grafico=0x7f06001c;
-        public static final int compara_estados=0x7f060025;
-        public static final int estados_opcao=0x7f060024;
-        public static final int hello_world=0x7f06003c;
-        public static final int ideb=0x7f06002f;
-        public static final int ideb_conteudo=0x7f060030;
-        public static final int jovens_pesquisadores=0x7f060022;
-        public static final int jovens_pesquisadores_investimento=0x7f060019;
-        public static final int jovens_pesquisadores_qtd=0x7f060018;
-        public static final int menu_botao_comparacao=0x7f060006;
-        public static final int menu_botao_consulta=0x7f06000a;
-        public static final int menu_botao_feed=0x7f06000b;
-        public static final int menu_botao_lista_estados=0x7f060005;
-        public static final int numero_projetos=0x7f06001e;
-        public static final int participacao_IDEB=0x7f06001f;
-        public static final int participacao_IDEB_fundamental_finais=0x7f060012;
-        public static final int participacao_IDEB_fundamental_iniciais=0x7f060011;
-        public static final int participacao_IDEB_medio=0x7f060013;
+        public static final int botao_tela_indicativos=0x7f05000c;
+        public static final int bt_grafico=0x7f05001c;
+        public static final int compara_estados=0x7f050025;
+        public static final int estados_opcao=0x7f050024;
+        public static final int hello_world=0x7f05003c;
+        public static final int ideb=0x7f05002f;
+        public static final int ideb_conteudo=0x7f050030;
+        public static final int jovens_pesquisadores=0x7f050022;
+        public static final int jovens_pesquisadores_investimento=0x7f050019;
+        public static final int jovens_pesquisadores_qtd=0x7f050018;
+        public static final int menu_botao_comparacao=0x7f050006;
+        public static final int menu_botao_consulta=0x7f05000a;
+        public static final int menu_botao_feed=0x7f05000b;
+        public static final int menu_botao_lista_estados=0x7f050005;
+        public static final int numero_projetos=0x7f05001e;
+        public static final int participacao_IDEB=0x7f05001f;
+        public static final int participacao_IDEB_fundamental_finais=0x7f050012;
+        public static final int participacao_IDEB_fundamental_iniciais=0x7f050011;
+        public static final int participacao_IDEB_medio=0x7f050013;
         /**  Strings da tela estado 
          */
-        public static final int participacao_PIB=0x7f06001d;
-        public static final int participacao_PIB_percentual=0x7f06000e;
-        public static final int participacao_percentual_PIB=0x7f06002b;
-        public static final int participacao_percentual_PIB_conteudo=0x7f06002c;
+        public static final int participacao_PIB=0x7f05001d;
+        public static final int participacao_PIB_percentual=0x7f05000e;
+        public static final int participacao_percentual_PIB=0x7f05002b;
+        public static final int participacao_percentual_PIB_conteudo=0x7f05002c;
         /**  Strings da Escolhe Indicativo Grafico 
          */
-        public static final int populacao=0x7f06000d;
-        public static final int primeiros_projetos=0x7f060031;
-        public static final int primeiros_projetos_conteudo=0x7f060032;
-        public static final int programa_primeiros_projetos_investimento=0x7f060015;
-        public static final int programa_primeiros_projetos_qtd=0x7f060014;
-        public static final int projeto_iniciacao=0x7f060023;
-        public static final int projeto_iniciacao_investimento=0x7f06001b;
-        public static final int projeto_iniciacao_qtd=0x7f06001a;
-        public static final int projetos_apoio_cnpq=0x7f060033;
-        public static final int projetos_apoio_cnpq_conteudo=0x7f060034;
-        public static final int projetos_difusao=0x7f06002d;
-        public static final int projetos_difusao_conteudo=0x7f06002e;
-        public static final int projetos_difusao_tecnologica_investimento=0x7f060010;
-        public static final int projetos_difusao_tecnologica_qtd=0x7f06000f;
-        public static final int projetos_inct=0x7f060037;
-        public static final int projetos_inct_conteudo=0x7f060038;
-        public static final int projetos_jovens_pesquisadores=0x7f060035;
-        public static final int projetos_jovens_pesquisadores_conteudo=0x7f060036;
-        public static final int projetos_pesquisa_CNPq_investimento=0x7f060017;
-        public static final int projetos_pesquisa_CNPq_qtd=0x7f060016;
-        public static final int string_vazia=0x7f060009;
-        public static final int string_x=0x7f060029;
-        public static final int tela_compara_estados=0x7f060027;
+        public static final int populacao=0x7f05000d;
+        public static final int primeiros_projetos=0x7f050031;
+        public static final int primeiros_projetos_conteudo=0x7f050032;
+        public static final int programa_primeiros_projetos_investimento=0x7f050015;
+        public static final int programa_primeiros_projetos_qtd=0x7f050014;
+        public static final int projeto_iniciacao=0x7f050023;
+        public static final int projeto_iniciacao_investimento=0x7f05001b;
+        public static final int projeto_iniciacao_qtd=0x7f05001a;
+        public static final int projetos_apoio_cnpq=0x7f050033;
+        public static final int projetos_apoio_cnpq_conteudo=0x7f050034;
+        public static final int projetos_difusao=0x7f05002d;
+        public static final int projetos_difusao_conteudo=0x7f05002e;
+        public static final int projetos_difusao_tecnologica_investimento=0x7f050010;
+        public static final int projetos_difusao_tecnologica_qtd=0x7f05000f;
+        public static final int projetos_inct=0x7f050037;
+        public static final int projetos_inct_conteudo=0x7f050038;
+        public static final int projetos_jovens_pesquisadores=0x7f050035;
+        public static final int projetos_jovens_pesquisadores_conteudo=0x7f050036;
+        public static final int projetos_pesquisa_CNPq_investimento=0x7f050017;
+        public static final int projetos_pesquisa_CNPq_qtd=0x7f050016;
+        public static final int string_vazia=0x7f050009;
+        public static final int string_x=0x7f050029;
+        public static final int tela_compara_estados=0x7f050027;
         /**  Strings da tela sobre 
          */
-        public static final int tela_sobre=0x7f06002a;
-        public static final int title_activity_aadsa=0x7f060043;
-        public static final int title_activity_feed_adapter=0x7f060041;
-        public static final int title_activity_grafico=0x7f060045;
-        public static final int title_activity_receptor_dados=0x7f060042;
-        public static final int title_activity_tela=0x7f06003b;
-        public static final int title_activity_tela_compara_estados=0x7f060026;
-        public static final int title_activity_tela_comparacao=0x7f060003;
-        public static final int title_activity_tela_consulta_indicativo=0x7f06003d;
-        public static final int title_activity_tela_escolhe_indicativo_grafico=0x7f060044;
-        public static final int title_activity_tela_estado=0x7f060002;
-        public static final int title_activity_tela_feed=0x7f060040;
-        public static final int title_activity_tela_grafico=0x7f060046;
-        public static final int title_activity_tela_indicativos_consultados=0x7f06003e;
-        public static final int title_activity_tela_lista_estado=0x7f060004;
-        public static final int title_activity_tela_resultado_consulta=0x7f06003f;
-        public static final int title_activity_tela_sobre=0x7f06003a;
-        public static final int titulo_escolhe_indicativo_grafico=0x7f060008;
-        public static final int titulo_menu=0x7f060028;
+        public static final int tela_sobre=0x7f05002a;
+        public static final int title_activity_aadsa=0x7f050043;
+        public static final int title_activity_feed_adapter=0x7f050041;
+        public static final int title_activity_grafico=0x7f050045;
+        public static final int title_activity_receptor_dados=0x7f050042;
+        public static final int title_activity_tela=0x7f05003b;
+        public static final int title_activity_tela_compara_estados=0x7f050026;
+        public static final int title_activity_tela_comparacao=0x7f050003;
+        public static final int title_activity_tela_consulta_indicativo=0x7f05003d;
+        public static final int title_activity_tela_escolhe_indicativo_grafico=0x7f050044;
+        public static final int title_activity_tela_estado=0x7f050002;
+        public static final int title_activity_tela_feed=0x7f050040;
+        public static final int title_activity_tela_grafico=0x7f050046;
+        public static final int title_activity_tela_indicativos_consultados=0x7f05003e;
+        public static final int title_activity_tela_lista_estado=0x7f050004;
+        public static final int title_activity_tela_resultado_consulta=0x7f05003f;
+        public static final int title_activity_tela_sobre=0x7f05003a;
+        public static final int titulo_escolhe_indicativo_grafico=0x7f050008;
+        public static final int titulo_menu=0x7f050028;
     }
     public static final class style {
-        /**  Application theme. 
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+ Application theme. 
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
@@ -332,32 +358,34 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070001;
-        /** 
+        public static final int AppBaseTheme=0x7f060000;
+        /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
 
     
          */
-        public static final int AppTheme=0x7f070000;
-        public static final int CustomProgressBar=0x7f070014;
-        public static final int botaoComparacao=0x7f070008;
-        public static final int botaoGeraGrfafico=0x7f070007;
-        public static final int botaoMenu=0x7f070006;
-        public static final int comparacao_subTitulo_1=0x7f07000a;
-        public static final int comparacao_subTitulo_2=0x7f07000d;
-        public static final int comparacao_titulo_1=0x7f07000b;
-        public static final int comparacao_titulo_2=0x7f07000e;
-        public static final int comparacao_valor_1=0x7f07000c;
-        public static final int comparacao_valor_2=0x7f07000f;
-        public static final int comparacao_x=0x7f070009;
-        public static final int spinner_item=0x7f070010;
-        public static final int spinner_item2=0x7f070011;
-        public static final int subTitulo=0x7f070002;
-        public static final int subTitulo2=0x7f070003;
-        public static final int subTituloSobre=0x7f070013;
-        public static final int titulo=0x7f070004;
-        public static final int tituloSobre=0x7f070012;
-        public static final int valor=0x7f070005;
+        public static final int AppTheme=0x7f060001;
+        public static final int CustomProgressBar=0x7f060014;
+        public static final int botaoComparacao=0x7f060008;
+        public static final int botaoGeraGrfafico=0x7f060007;
+        public static final int botaoMenu=0x7f060006;
+        public static final int comparacao_subTitulo_1=0x7f06000a;
+        public static final int comparacao_subTitulo_2=0x7f06000d;
+        public static final int comparacao_titulo_1=0x7f06000b;
+        public static final int comparacao_titulo_2=0x7f06000e;
+        public static final int comparacao_valor_1=0x7f06000c;
+        public static final int comparacao_valor_2=0x7f06000f;
+        public static final int comparacao_x=0x7f060009;
+        public static final int spinner_item=0x7f060010;
+        public static final int spinner_item2=0x7f060011;
+        public static final int subTitulo=0x7f060002;
+        public static final int subTitulo2=0x7f060003;
+        public static final int subTituloSobre=0x7f060013;
+        public static final int titulo=0x7f060004;
+        public static final int tituloSobre=0x7f060012;
+        public static final int valor=0x7f060005;
     }
 }
--- EEF/src/com/mdsgpp/eef/visao/TelaEscolheIndicativoGrafico.java
+++ EEF/src/com/mdsgpp/eef/visao/TelaEscolheIndicativoGrafico.java
@@ -10,8 +10,8 @@ import android.view.View;
 import android.widget.RadioButton;
 
 public class TelaEscolheIndicativoGrafico extends Activity {
-	private String indicativo = "valorpadrao";
-	private String titulo = "valorpadrao";
+	private String indicativo = "populacao";
+	private String titulo = "População";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,64 +27,94 @@ public class TelaEscolheIndicativoGrafico extends Activity {
 	}
 	
 	
-	public void onRadioButtonClicked(View view) {
+public void onRadioButtonClicked(View view) {
 		
         boolean checked = ((RadioButton) view).isChecked();
         switch(view.getId()) {
             case R.id.radio_apoio_cnpq_investimento:
                 if (checked)
+                	indicativo = "valor_projetos_cnpq";
+                	titulo = "Projetos de Pesquisa Apoio CNPq (R$)";
                 break;
             case R.id.radio_apoio_cnpq_qtd:
                 if (checked)
+                	indicativo = "quantidade_projeto_cnpq";
+                	titulo = "Projetos de Pesquisa Apoio CNPq (Qtd.)";
                 break;
             case R.id.radio_difusao_tecnologica_investimento:
                 if (checked)
+                	indicativo = "valor_ciencia_tecnologia";
+                	titulo = "Projeto de Difusão Tecnológica (R$)";
                 break;
             case R.id.radio_difusao_tecnologica_qtd:
                 if (checked)
+                	indicativo = "projetos_ciencia_tecnologia";
+                	titulo = "Projeto de Difusão Tecnológica (Qtd.)";
                 break;
             case R.id.radio_ideb_fundamental_finais:
                 if (checked)
+                	indicativo = "ideb_fundamental_final";
+                	titulo = "IDEB do Ensino Fundamental (Séries Finais)";
                 break;
             case R.id.radio_ideb_fundamental_iniciai:
                 if (checked)
+                	indicativo = "ideb_fundamental_inicial";
+                titulo = "IDEB do Ensino Fundamental (Séries Iniciais)";
                 break;
             case R.id.radio_ideb_medio:
                 if (checked)
+                	indicativo = "ideb_ensino_medio";
+                	titulo = "IDEB do Ensino Médio";
                 break;
             case R.id.radio_jovens_pesquisadores_investimento:
                 if (checked)
+                	indicativo = "valor_projetos_jovens_pesquisadores";
+                	titulo = "Jovens pesquisadores (R$)";
                 break;
                 
             case R.id.radio_jovens_pesquisadores_qtd:
                 if (checked)
+                	indicativo = "quantidade_projeto_jovens_pesquisadores";
+                	titulo = "Jovens pesquisadores (Qtd.)";
                 break;
                 
             case R.id.radio_pib:
                 if (checked)
+                	indicativo = "percentual_participacao_pib";
+                	titulo = "Participação Estadual no PIB (%)";
                 break;
                 
             case R.id.radio_populacao:
                 if (checked)
+                	indicativo = "populacao";
+                	titulo = "População";
                 break;
                 
             case R.id.radio_primeiros_projetos_investimento:
                     if (checked)
+                    	indicativo = "valor_primeiros_projetos";
+                    	titulo = "Programa Primeiros Projetos (R$)";
                     break;
                     
                 case R.id.radio_primeiros_projetos_qtd:
                     if (checked)
+                    	indicativo = "quantidade_primeiros_projetos";
+                    	titulo = "Programa Primeiros Projetos (Qtd.)";
                     break;
                     
                 case R.id.radio_projetos_inct_investimento:
                     if (checked)
+                    	indicativo = "valor_projetos_inct";
+                    	titulo = "Projetos INCT (R$)";
                     break;
                     
                 case R.id.radio_projetos_inct_qtd:
                     if (checked)
+                    	indicativo = "quantidade_projetos_inct";
+                    	titulo = "Projetos INCT (Qtd.)";
                     break;
         }
-        }
+	}
 	
 	public void clickBotaoGrafico(View view) {
 		Intent intent = new Intent(this, TelaGrafico.class);
