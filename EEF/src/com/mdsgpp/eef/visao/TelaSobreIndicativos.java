ddbda9b0e64ae0d79f2c06987c2b4bfc0382c638
BrunaNayara <brunanayaramlima@gmail.com>
2013-11-19 13:57:24 -0200

    Adicionando Classe Tela Sobre Indicativos. Piloto: Bruna Co-piloto: Matheus

---
+++ EEF/src/com/mdsgpp/eef/visao/TelaSobreIndicativos.java
@@ -0,0 +1,25 @@
+package com.mdsgpp.eef.visao;
+
+import com.mdsgpp.eef.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class TelaSobreIndicativos extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tela_sobre);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tela_sobre, menu);
+		return true;
+	}
+
+}
