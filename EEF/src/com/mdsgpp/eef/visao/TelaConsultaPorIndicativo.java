343919c3d904fcd499cc1d12b0a8098fce1b97dd
vanusa <vanusa_marcelino@yahoo.com.br>
2013-11-22 16:36:22 -0200

    refatoração da tela consulta por indicativo copiloto: Marcos

--- EEF/src/com/mdsgpp/eef/visao/TelaConsultaPorIndicativo.java
+++ EEF/src/com/mdsgpp/eef/visao/TelaConsultaPorIndicativo.java
@@ -23,7 +23,7 @@ public class TelaConsultaPorIndicativo extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.tela_consulta_indicativo, menu);
+		getMenuInflater().inflate(R.menu.tela_consulta_por_indicativo, menu);
 		return true;
 	}
 	
