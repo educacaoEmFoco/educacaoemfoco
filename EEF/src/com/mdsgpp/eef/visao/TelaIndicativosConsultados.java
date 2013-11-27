8e83c3c4a408ec63d97ca8b1af2d709595de34ba
Vanusa <vanusa_marcelino@yahoo.com.br>
2013-11-27 14:41:05 -0200

    Implementacao das opcaoes de marcar e desmarcar todos os indicativos na TelaIndicativosConsultados. Piloto:Vanusa Co-piloto:Bruna

--- EEF/src/com/mdsgpp/eef/visao/TelaIndicativosConsultados.java
+++ EEF/src/com/mdsgpp/eef/visao/TelaIndicativosConsultados.java
@@ -8,14 +8,17 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.RadioButton;
 
 public class TelaIndicativosConsultados extends Activity {
-	
+
 	CheckBox cbIdeb, cbPib, cbPopulacao, cbPrimeirosProjetos, cbProjetosCnpq,
-			cbProjetosDifusao, cbProjetosIniciacao, cbProjetosJovens, cbCenso, cbAlunosTurma, cbHorasAula, cbTaxaDistorcao, cbTaxaAbandono, cbAprovacao;
+			cbProjetosDifusao, cbProjetosIniciacao, cbProjetosJovens, cbCenso,
+			cbAlunosTurma, cbHorasAula, cbTaxaDistorcao, cbTaxaAbandono,
+			cbAprovacao;
 	boolean bIdeb, bPib, bPopulacao, bPrimeirosProjetos, bProjetosCnpq,
-			bProjetosDifusao, bProjetosIniciacao, bProjetosJovens, bCenso, bAlunosTurma, bHorasAula, bTaxaDistorcao, bTaxaAbandono, bAprovacao;
+			bProjetosDifusao, bProjetosIniciacao, bProjetosJovens, bCenso,
+			bAlunosTurma, bHorasAula, bTaxaDistorcao, bTaxaAbandono,
+			bAprovacao;
 	int posicao01, posicao02;
 
 	@Override
@@ -23,11 +26,11 @@ public class TelaIndicativosConsultados extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela_indicativos_consultados);
 		Intent intentAuxiliar = getIntent();
-		
+
+		inicializaCheckBox();
+
 		posicao01 = intentAuxiliar.getIntExtra("INDEX_ESTADO1_ESCOLHIDO", 0);
-		
 		posicao02 = intentAuxiliar.getIntExtra("INDEX_ESTADO2_ESCOLHIDO", 0);
-		
 	}
 
 	@Override
@@ -36,9 +39,7 @@ public class TelaIndicativosConsultados extends Activity {
 		getMenuInflater().inflate(R.menu.tela_indicativos_consultados, menu);
 		return true;
 	}
-	
-	
-	
+
 	private void inicializaCheckBox() {
 		cbIdeb = (CheckBox) findViewById(R.id.checkBox_ideb);
 		cbPib = (CheckBox) findViewById(R.id.checkBox_participacao_pib);
@@ -48,7 +49,6 @@ public class TelaIndicativosConsultados extends Activity {
 		cbProjetosDifusao = (CheckBox) findViewById(R.id.checkBox_projetos_difusao);
 		cbProjetosIniciacao = (CheckBox) findViewById(R.id.checkBox_projetos_iniciacao);
 		cbProjetosJovens = (CheckBox) findViewById(R.id.checkBox_projetos_jovens);
-		
 		cbCenso = (CheckBox) findViewById(R.id.checkBox_censo);
 		cbAlunosTurma = (CheckBox) findViewById(R.id.checkBox_quantidade_aluno);
 		cbHorasAula = (CheckBox) findViewById(R.id.checkBox_horas_aula);
@@ -56,23 +56,7 @@ public class TelaIndicativosConsultados extends Activity {
 		cbTaxaAbandono = (CheckBox) findViewById(R.id.checkBox_taxa_abandono);
 		cbAprovacao = (CheckBox) findViewById(R.id.checkBox_taxa_aprovacao);
 	}
-	
-	
-public void onRadioButtonClicked(View view) {
-		
-        boolean checked = ((RadioButton) view).isChecked();
-        switch(view.getId()) {
-            case R.id.radioButtonMarcar:
-                if (checked)
-                	
-                break;
-            case R.id.radioButtonDesmarcar:
-                if (checked)
-                	
-                break;
-        }
-}
-	
+
 	private void capturaValores() {
 		bIdeb = cbIdeb.isChecked();
 		bPib = cbPib.isChecked();
@@ -82,21 +66,43 @@ public void onRadioButtonClicked(View view) {
 		bProjetosDifusao = cbProjetosDifusao.isChecked();
 		bProjetosIniciacao = cbProjetosIniciacao.isChecked();
 		bProjetosJovens = cbProjetosJovens.isChecked();
-		
 		bCenso = cbCenso.isChecked();
 		bAlunosTurma = cbAlunosTurma.isChecked();
 		bHorasAula = cbHorasAula.isChecked();
 		bTaxaDistorcao = cbTaxaDistorcao.isChecked();
 		bTaxaAbandono = cbTaxaAbandono.isChecked();
 		bAprovacao = cbAprovacao.isChecked();
-		
+	}
+	
+	public void marcaOuDesmarcaCheckBox(boolean opcaoEscolhida) {
+		cbIdeb.setChecked(opcaoEscolhida);
+		cbPib.setChecked(opcaoEscolhida);
+		cbPopulacao.setChecked(opcaoEscolhida);
+		cbPrimeirosProjetos.setChecked(opcaoEscolhida);
+		cbProjetosCnpq.setChecked(opcaoEscolhida);
+		cbProjetosDifusao.setChecked(opcaoEscolhida);
+		cbProjetosIniciacao.setChecked(opcaoEscolhida);
+		cbProjetosJovens.setChecked(opcaoEscolhida);
+		cbCenso.setChecked(opcaoEscolhida);
+		cbAlunosTurma.setChecked(opcaoEscolhida);
+		cbHorasAula.setChecked(opcaoEscolhida);
+		cbTaxaDistorcao.setChecked(opcaoEscolhida);
+		cbTaxaAbandono.setChecked(opcaoEscolhida);
+		cbAprovacao.setChecked(opcaoEscolhida);
+	}
+	
+	public void onRadioButtonMarcarTodosClicked(View view) {
+		marcaOuDesmarcaCheckBox(true);
+	}
+
+	public void onRadioButtonDesmarcarTodosClicked(View view) {
+		marcaOuDesmarcaCheckBox(false);
 	}
 	
 	public void clickBotao(View view) {
-		inicializaCheckBox();
 		capturaValores();
-		
-		Intent intent = new Intent(this,TelaResultadoConsulta.class);
+
+		Intent intent = new Intent(this, TelaResultadoConsulta.class);
 		intent.putExtra("CB_IDEB", bIdeb);
 		intent.putExtra("CB_PIB", bPib);
 		intent.putExtra("CB_POPULACAO", bPopulacao);
@@ -105,7 +111,6 @@ public void onRadioButtonClicked(View view) {
 		intent.putExtra("CB_PROJETOS_DIFUSAO", bProjetosDifusao);
 		intent.putExtra("CB_PROJETOS_INICIACAO", bProjetosIniciacao);
 		intent.putExtra("CB_PROJETOS_JOVENS", bProjetosJovens);
-		
 		intent.putExtra("CB_ALUNOS_TURMA", bAlunosTurma);
 		intent.putExtra("CB_APROVACAO", bAprovacao);
 		intent.putExtra("CB_CENSO", bCenso);
@@ -113,11 +118,10 @@ public void onRadioButtonClicked(View view) {
 		intent.putExtra("CB_TAXA_ABANDONO", bTaxaAbandono);
 		intent.putExtra("CB_TAXA_DISTORCAO", bTaxaDistorcao);
 		
-		intent.putExtra("INDEX_ESTADO1_ESCOLHIDO",posicao01 );
-		intent.putExtra("INDEX_ESTADO2_ESCOLHIDO",posicao02 );
-		
+		intent.putExtra("INDEX_ESTADO1_ESCOLHIDO", posicao01);
+		intent.putExtra("INDEX_ESTADO2_ESCOLHIDO", posicao02);
+
 		startActivity(intent);
-		
 	}
 
 }
