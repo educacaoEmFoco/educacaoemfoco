ea759741f5d9d19a697953f3d1140e6e94fc0ecf
Ludimila Bela <lbelacruz@gmail.com>
2013-11-28 00:14:14 -0200

    correcao ortografica do nome da classe

---
+++ EEF/src/com/mdsgpp/eef/testes/NoticiasTeste.java
@@ -0,0 +1,72 @@
+package com.mdsgpp.eef.testes;
+
+import android.test.AndroidTestCase;
+
+import com.mdsgpp.eef.modelo.Feed;
+import com.mdsgpp.eef.modelo.Noticias;
+
+public class NoticiasTeste extends AndroidTestCase {
+
+	private String title;
+	private String category;
+	private String description;
+	private String link;
+	private String date; 
+	private Feed feed;
+	private Noticias noticias;
+	
+	
+	public void setUp(){
+		feed = new Feed();
+		noticias = new Noticias(feed);
+		category = "categoria";
+		title = "title";
+		description = "description";
+		link = "link";
+		date = "date";
+		
+	}
+	
+	
+	
+	public void testTitle() {
+		noticias.setTitle(title);
+		assertEquals(title,noticias.getTitle());
+		
+	}
+	
+	
+	public void testCategory() {
+		noticias.setCategory(category);
+		assertEquals(category,noticias.getCategory());
+		
+	}
+	
+
+	public void testDescription() {
+		noticias.setDescription(description);
+		assertEquals(description,noticias.getDescription());
+		
+	}
+	
+	
+	public void testLink() {
+		noticias.setLink(link);
+		assertEquals(link,noticias.getLink());
+		
+	}
+
+	
+	public void testDate() {
+		noticias.setDate(date);
+		assertEquals(date,noticias.getDate());
+		
+	}
+	
+	
+	public void testGetFeed() {
+		
+		assertEquals(feed,noticias.getFeed());
+	}
+
+}
