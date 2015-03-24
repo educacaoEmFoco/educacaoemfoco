55d03d94d9cfba0e2ebfb26753356b586d14b08b
Makrau <iago006@hotmail.com>
2015-03-24 10:41:53 -0300

    Indentation: holographlibrary/Bar

--- EEF/src/com/echo/holographlibrary/Bar.java
+++ EEF/src/com/echo/holographlibrary/Bar.java
@@ -27,55 +27,62 @@ import android.graphics.Path;
 import android.graphics.Region;
 
 public class Bar {
-	private int mColor;
+	private int mColor = 0;
 	private String mName = null;
-	private float mValue;
 	private String mValueString = null;
+	private float mValue = 0;
 	private Path mPath = null;
 	private Region mRegion = null;
 	
 	public int getColor() {
 		return mColor;
 	}
+	
 	public void setColor(int color) {
 		this.mColor = color;
 	}
+	
 	public String getName() {
 		return mName;
 	}
+	
 	public void setName(String name) {
 		this.mName = name;
 	}
+	
 	public float getValue() {
 		return mValue;
 	}
+	
 	public void setValue(float value) {
 		this.mValue = value;
 	}
 	
-	public String getValueString()
-	{
-		if (mValueString != null) {
+	public String getValueString() {
+		if(mValueString != null) {
 			return mValueString;
-		} else {
+		} 
+		else {
 			return String.valueOf(mValue);
 		}
 	}
 	
-	public void setValueString(final String valueString)
-	{
+	public void setValueString(final String valueString) {
 		mValueString = valueString;
 	}
 	
 	public Path getPath() {
 		return mPath;
 	}
+	
 	public void setPath(Path path) {
 		this.mPath = path;
 	}
+	
 	public Region getRegion() {
 		return mRegion;
 	}
+	
 	public void setRegion(Region region) {
 		this.mRegion = region;
 	}
