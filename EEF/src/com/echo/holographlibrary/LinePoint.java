c34ce23e96ddc4b0e30df23b8d355d16736bb460
Makrau <iago006@hotmail.com>
2015-03-30 11:17:21 -0300

    Indentation: holographlibrary/LinePoint

--- EEF/src/com/echo/holographlibrary/LinePoint.java
+++ EEF/src/com/echo/holographlibrary/LinePoint.java
@@ -29,35 +29,39 @@ import android.graphics.Region;
 public class LinePoint {
 	private float x = 0;
 	private float y = 0;
-	private Path path;
-	private Region region;
+	private Path path = null;
+	private Region region = null;
 
 	
 	public float getX() {
 		return x;
 	}
+	
 	public void setX(float x) {
 		this.x = x;
 	}
+	
 	public float getY() {
 		return y;
 	}
+	
 	public void setY(float y) {
 		this.y = y;
 	}
+	
 	public Region getRegion() {
 		return region;
 	}
+	
 	public void setRegion(Region region) {
 		this.region = region;
 	}
+	
 	public Path getPath() {
 		return path;
 	}
+	
 	public void setPath(Path path) {
 		this.path = path;
 	}
-	
-	
-	
 }
