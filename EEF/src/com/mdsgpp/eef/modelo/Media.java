0458fa88b8e3173857a76f37016c063c482290cf
Makrau <iago006@hotmail.com>
2015-03-30 20:56:42 -0300

    Indentation: modelo/Media

--- EEF/src/com/mdsgpp/eef/modelo/Media.java
+++ EEF/src/com/mdsgpp/eef/modelo/Media.java
@@ -1,49 +1,49 @@
 package com.mdsgpp.eef.modelo;
 
 public class Media {
-		private double ensinoFundamental;
-		private double ensinoMedio;
-		private int ano;
-		private Estado estado;
+	private double ensinoFundamental;
+	private double ensinoMedio;
+	private int ano;
+	private Estado estado;
 		
-		public Media() {
-		}
+	public Media() {
+	}
 		
-		public Media(double ensinoFundamental, double ensinoMedio){
-			this.ensinoFundamental = ensinoFundamental;
-			this.ensinoMedio = ensinoMedio;
-		}
+	public Media(double ensinoFundamental, double ensinoMedio){
+		this.ensinoFundamental = ensinoFundamental;
+		this.ensinoMedio = ensinoMedio;
+	}
 		
-		public double getEnsinoFundamental() {
-			return ensinoFundamental;
-		}
+	public double getEnsinoFundamental() {
+		return ensinoFundamental;
+	}
 
-		public void setEnsinoFundamental(double ensinoFundamental) {
-			this.ensinoFundamental = ensinoFundamental;
-		}
+	public void setEnsinoFundamental(double ensinoFundamental) {
+		this.ensinoFundamental = ensinoFundamental;
+	}
 
-		public double getEnsinoMedio() {
-			return ensinoMedio;
-		}
+	public double getEnsinoMedio() {
+		return ensinoMedio;
+	}
 
-		public void setEnsinoMedio(double ensinoMedio) {
-			this.ensinoMedio = ensinoMedio;
-		}
+	public void setEnsinoMedio(double ensinoMedio) {
+		this.ensinoMedio = ensinoMedio;
+	}
 
-		public Estado getEstado() {
-			return estado;
-		}
+	public Estado getEstado() {
+		return estado;
+	}
 
-		public void setEstado(Estado estado) {
-			this.estado = estado;
-		}
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
 
-		public int getAno() {
-			return ano;
-		}
+	public int getAno() {
+		return ano;
+	}
 
-		public void setAno(int ano) {
-			this.ano = ano;
-		}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
 		
 }
