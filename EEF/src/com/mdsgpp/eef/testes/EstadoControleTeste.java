716fb455159a24b05a95b7868195b20bb46a10ce
Jonathan Rufino <jonathan_rufinotkd@hotmail.com>
2015-04-05 22:18:55 -0300

    Indentation: testes/EstadoControleTeste

--- EEF/src/com/mdsgpp/eef/testes/EstadoControleTeste.java
+++ EEF/src/com/mdsgpp/eef/testes/EstadoControleTeste.java
@@ -8,7 +8,7 @@ import com.mdsgpp.eef.controle.EstadoControle;
 
 import android.test.AndroidTestCase;
 
-public class EstadoControleTeste extends AndroidTestCase{
+public class EstadoControleTeste extends AndroidTestCase {
 
 	private HashMap<String, String> informacoes;
 	private HashMap<String, String> informacoesCompletas;
@@ -31,23 +31,32 @@ public class EstadoControleTeste extends AndroidTestCase{
 		
 		this.informacoes.put("sigla", "AC");
 		this.informacoes.put("nome", "Acre");
-		this.informacoes.put("populacao",  dfPopulacao.format(691132) + " habitantes");
-		this.informacoes.put("percentual_participacao_pib", dfPorcentagem.format(0.200000000000045)+ "%");
-		this.informacoes.put("projetos_ciencia_tecnologia","Quantidade: " + 3 + " projetos");
-		this.informacoes.put("valor_ciencia_tecnologia", "Valor investido: R$ " + dfValor.format(563681.899999976)+ " (em mil)");
-		this.informacoes.put("ideb_fundamental_final", "Ensino Fundamental (s�ries finais): "+dfPorcentagem.format(4.09999999999854));
-		this.informacoes.put("ideb_ensino_medio","Ensino M�dio: " + dfPorcentagem.format(3.5) + "" );	
-		this.informacoes.put("ideb_fundamental_inicial", "Ensino Fundamental (S�ries iniciais): " + dfPorcentagem.format(4.5) + "");
-		this.informacoes.put("quantidade_primeiros_projetos", "Quantidade: "+ 2 + " projetos");
-		this.informacoes.put("valor_primeiros_projetos", "Valor investido: R$ "+ dfValor.format(55.4400000000023) + " (em mil)");
-		this.informacoes.put("quantidade_projeto_cnpq","Quantidade: " + 133 + " projetos");
-		this.informacoes.put("valor_projetos_cnpq","Valor investido: R$ " + dfValor.format( 1905.13000000053 ) + " (em mil)");
-		this.informacoes.put("quantidade_projeto_jovens_pesquisadores","Quantidade: " + 3 + " projetos");
-		this.informacoes.put("valor_projetos_jovens_pesquisadores","Valor investido: R$ " + dfValor.format( 138.250000000029 ) + " (em mil)");
+		this.informacoes.put("populacao", dfPopulacao.format(691132) + " habitantes");
+		this.informacoes.put("percentual_participacao_pib", 
+			dfPorcentagem.format(0.200000000000045) + "%");
+		this.informacoes.put("projetos_ciencia_tecnologia", "Quantidade: " + 3 + " projetos");
+		this.informacoes.put("valor_ciencia_tecnologia", "Valor investido: R$ " 
+			+ dfValor.format(563681.899999976) + " (em mil)");
+		this.informacoes.put("ideb_fundamental_final", "Ensino Fundamental (séries finais): " 
+			+ dfPorcentagem.format(4.09999999999854));
+		this.informacoes.put("ideb_ensino_medio", "Ensino Médio: " + dfPorcentagem.format(3.5) 
+			+ "" );	
+		this.informacoes.put("ideb_fundamental_inicial", "Ensino Fundamental (Séries iniciais): " 
+			+ dfPorcentagem.format(4.5) + "");
+		this.informacoes.put("quantidade_primeiros_projetos", "Quantidade: " + 2 + " projetos");
+		this.informacoes.put("valor_primeiros_projetos", "Valor investido: R$ " 
+			+ dfValor.format(55.4400000000023) + " (em mil)");
+		this.informacoes.put("quantidade_projeto_cnpq", "Quantidade: " + 133 + " projetos");
+		this.informacoes.put("valor_projetos_cnpq", "Valor investido: R$ " 
+			+ dfValor.format( 1905.13000000053 ) + " (em mil)");
+		this.informacoes.put("quantidade_projeto_jovens_pesquisadores", "Quantidade: " + 3 
+			+ " projetos");
+		this.informacoes.put("valor_projetos_jovens_pesquisadores", "Valor investido: R$ " 
+			+ dfValor.format( 138.250000000029 ) + " (em mil)");
 		
 		this.informacoesCompletas.put("sigla", "AC");
 		this.informacoesCompletas.put("nome", "Acre");
-		this.informacoesCompletas.put("populacao",  dfPopulacao.format(691132) + " habitantes");
+		this.informacoesCompletas.put("populacao", dfPopulacao.format(691132) + " habitantes");
 	}
 
 	public void tearDown() throws Exception {
@@ -55,58 +64,63 @@ public class EstadoControleTeste extends AndroidTestCase{
 	}
 
 	public void testLerEstado() throws IOException {
-		assertEquals(this.informacoes.get("nome"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("nome"));
+		assertEquals(this.informacoes.get("nome"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get("nome"));
 
-		assertEquals(this.informacoes.get("sigla"),
-					 EstadoControle.getInstancia(getContext()).lerEstado(0).get("sigla"));
+		assertEquals(this.informacoes.get("sigla"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get("sigla"));
 
-		assertEquals(this.informacoes.get("populacao"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("populacao"));
+		assertEquals(this.informacoes.get("populacao"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get("populacao"));
 
-		assertEquals(this.informacoes.get("percentual_participacao_pib"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("percentual_participacao_pib"));
+		assertEquals(this.informacoes.get("percentual_participacao_pib"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get
+				("percentual_participacao_pib"));
 		
-		assertEquals(this.informacoes.get("projetos_ciencia_tecnologia"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("projetos_ciencia_tecnologia"));
+		assertEquals(this.informacoes.get("projetos_ciencia_tecnologia"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get
+				("projetos_ciencia_tecnologia"));
 		
-		assertEquals(this.informacoes.get("ideb_fundamental_final"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("ideb_fundamental_final"));
+		assertEquals(this.informacoes.get("ideb_fundamental_final"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get("ideb_fundamental_final"));
 		
-		assertEquals(this.informacoes.get("ideb_ensino_medio"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("ideb_ensino_medio"));
+		assertEquals(this.informacoes.get("ideb_ensino_medio"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get("ideb_ensino_medio"));
 		
-		assertEquals(this.informacoes.get("ideb_fundamental_inicial"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("ideb_fundamental_inicial"));
+		assertEquals(this.informacoes.get("ideb_fundamental_inicial"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get("ideb_fundamental_inicial"));
 
-		assertEquals(this.informacoes.get("quantidade_primeiros_projetos"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("quantidade_primeiros_projetos"));
+		assertEquals(this.informacoes.get("quantidade_primeiros_projetos"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get
+				("quantidade_primeiros_projetos"));
 
-		assertEquals(this.informacoes.get("valor_primeiros_projetos"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("valor_primeiros_projetos"));
+		assertEquals(this.informacoes.get("valor_primeiros_projetos"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get("valor_primeiros_projetos"));
 
-		assertEquals(this.informacoes.get("quantidade_projeto_cnpq"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("quantidade_projeto_cnpq"));
+		assertEquals(this.informacoes.get("quantidade_projeto_cnpq"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get("quantidade_projeto_cnpq"));
 
-		assertEquals(this.informacoes.get("valor_projetos_cnpq"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("valor_projetos_cnpq"));
+		assertEquals(this.informacoes.get("valor_projetos_cnpq"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get("valor_projetos_cnpq"));
 
-		assertEquals(this.informacoes.get("quantidade_projeto_jovens_pesquisadores"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("quantidade_projeto_jovens_pesquisadores"));
+		assertEquals(this.informacoes.get("quantidade_projeto_jovens_pesquisadores"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get
+				("quantidade_projeto_jovens_pesquisadores"));
 
-		assertEquals(this.informacoes.get("valor_projetos_jovens_pesquisadores"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstado(0).get("valor_projetos_jovens_pesquisadores"));
+		assertEquals(this.informacoes.get("valor_projetos_jovens_pesquisadores"), 
+			EstadoControle.getInstancia(getContext()).lerEstado(0).get
+				("valor_projetos_jovens_pesquisadores"));
 	}
 	
 	public void testLerEstadoCompleto() throws IOException {
-		assertEquals(this.informacoesCompletas.get("nome"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstadoCompleto(0).get("nome"));
+		assertEquals(this.informacoesCompletas.get("nome"), 
+			EstadoControle.getInstancia(getContext()).lerEstadoCompleto(0).get("nome"));
 
-		assertEquals(this.informacoesCompletas.get("sigla"),
-					 EstadoControle.getInstancia(getContext()).lerEstadoCompleto(0).get("sigla"));
+		assertEquals(this.informacoesCompletas.get("sigla"), 
+			EstadoControle.getInstancia(getContext()).lerEstadoCompleto(0).get("sigla"));
 		
-		assertEquals(this.informacoesCompletas.get("populacao"),
-				 	 EstadoControle.getInstancia(getContext()).lerEstadoCompleto(0).get("populacao"));
+		assertEquals(this.informacoesCompletas.get("populacao"), 
+			EstadoControle.getInstancia(getContext()).lerEstadoCompleto(0).get("populacao"));
 	}
 
 	public void testInstancia() throws IOException {
