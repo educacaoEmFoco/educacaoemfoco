0d8c4cde20bbd539b8f31f53f9abf12d47ba6077
Makrau <iago006@hotmail.com>
2015-04-12 16:19:44 -0300

    English Translation: visao/DataReceiver

--- EEF/src/com/mdsgpp/eef/visao/DataReceiver.java
+++ EEF/src/com/mdsgpp/eef/visao/DataReceiver.java
@@ -2,7 +2,7 @@ package com.mdsgpp.eef.visao;
 
 import com.mdsgpp.eef.modelo.Feed;
 
-public interface ReceptorDados {
+public interface DataReceiver {
 
 	// Receive the information about states.
 	void receiveFeed(Feed feed);	
