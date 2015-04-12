3f6aa2ca583ebb54710d1cd6af22a00420a84966
Lucas Mattioli <lucas.mattioli@live.com>
2015-04-12 20:23:54 -0300

    English Translation: tests/FeedPersistenceTest

--- EEF/src/com/mdsgpp/eef/tests/FeedPersistenceTest.java
+++ EEF/src/com/mdsgpp/eef/tests/FeedPersistenceTest.java
@@ -2,36 +2,36 @@ package com.mdsgpp.eef.tests;
 
 import java.io.IOException;
 
-import com.mdsgpp.eef.modelo.Feed;
-import com.mdsgpp.eef.modelo.Noticias;
+import com.mdsgpp.eef.model.Feed;
+import com.mdsgpp.eef.model.News;
 import com.mdsgpp.eef.parser.FeedPersistence;
 
 import android.content.Context;
 import android.test.AndroidTestCase;
 
-public class FeedPersistenciaTeste extends AndroidTestCase {
+public class FeedPersistenceTest extends AndroidTestCase {
 	private Feed feed;
 	private Context context;
-	private Noticias noticia;
+	private News news;
 
 	protected void setUp() throws Exception {
 		super.setUp();
 		feed = new Feed();
-		noticia = new Noticias(feed);
-		noticia.setTitle("Teste de persistencia");
-		noticia.setCategory("teste");
-		noticia.setDate("18/02");
-		noticia.setLink("www.noticias.gov");
-		noticia.setDescription("Descricao");
+		news = new News(feed);
+		news.setTitle("Teste de persistencia");
+		news.setCategory("teste");
+		news.setDate("18/02");
+		news.setLink("www.newss.gov");
+		news.setDescription("Descricao");
 
-		feed.addItem(noticia);
+		feed.addItem(news);
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
 
-	public void testgetinstancia() {
+	public void testGetInstance() {
 		context = getContext();
 		String nomeClasseRetorno = FeedPersistence.getInstance(context).getClass().getSimpleName();
 		assertEquals("FeedPersistencia", nomeClasseRetorno);
@@ -47,7 +47,7 @@ public class FeedPersistenciaTeste extends AndroidTestCase {
 		assertEquals("Título esperado", "Teste de persistencia", feed.getItem(0).getTitle());
 		assertEquals("Categoria esperada", "teste", feed.getItem(0).getCategory());
 		assertEquals("Data esperada", "18/02", feed.getItem(0).getDate());
-		assertEquals("Link esperado", "www.noticias.gov", feed.getItem(0).getLink());
+		assertEquals("Link esperado", "www.newss.gov", feed.getItem(0).getLink());
 		assertEquals("Descricao esperada", "Descricao", feed.getItem(0).getDescription());
 	}
 }
