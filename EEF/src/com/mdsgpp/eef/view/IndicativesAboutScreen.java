415cae0261047445291545e90266ddbc656f0cbe
Makrau <iago006@hotmail.com>
2015-04-16 11:27:54 -0300

    English Translation: view/ IndicativesAboutScreen

--- EEF/src/com/mdsgpp/eef/view/IndicativesAboutScreen.java
+++ EEF/src/com/mdsgpp/eef/view/IndicativesAboutScreen.java
@@ -1,4 +1,4 @@
-package com.mdsgpp.eef.visao;
+package com.mdsgpp.eef.view;
 
 import com.mdsgpp.eef.R;
 
@@ -8,7 +8,7 @@ import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class TelaSobreIndicativos extends Activity {
+public class IndicativesAboutScreen extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
