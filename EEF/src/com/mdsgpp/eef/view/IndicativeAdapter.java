25e6b3778d60383666feea5c34691ac185186070
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2015-04-30 21:14:12 -0300

    Uncomplicating: view/IndicatiAdapter

--- EEF/src/com/mdsgpp/eef/view/IndicativeAdapter.java
+++ EEF/src/com/mdsgpp/eef/view/IndicativeAdapter.java
@@ -22,99 +22,110 @@ import android.widget.TextView;
 import android.view.Gravity;
 
 public class IndicativeAdapter extends BaseAdapter {
-	
-	private HashMap<String, String> state;
-	private String choosenIndicative;
-	private String title;
-	private Context context;
-	private ViewHolder holder;
-	private LayoutInflater inflater;
-	
+
+	private HashMap<String, String> state = null;
+	private String choosenIndicative = null;
+	private String title = null;
+	private Context context = null;
+	private ViewHolder viewHolder = null;
+	private LayoutInflater layoutInflater = null;
+	final int amountMenuItens = 28;
+	final int titlePosition = 1;
+
 	// Contains the name of all states from brazil, to select its own flag.
-	String flags[] = {"acre", "alagoas", "amapa", "amazonas", "bahia", "ceara", 
+	final String flagsNames[] = {"acre", "alagoas", "amapa", "amazonas", "bahia", "ceara", 
 		"distritofederal", "espiritosanto", "goias", "maranhao", "matogrosso", "matogrossodosul", 
 		"minasgerais", "para", "paraiba", "parana", "pernambuco", "piaui", "riodejaneiro", 
 		"riograndedonorte", "riograndedosul", "rondonia", "roraima", "santacatarina", "saopaulo", 
 		"sergipe", "tocantins"};
-	
+
 	static class ViewHolder{
-		private TextView tvName;
-		private TextView tvIndicativeValue;
-		private ImageView tvFlags;
+		private TextView textViewName;
+		private TextView textViewIndicativeValue;
+		private ImageView textViewFlags;
 	}
-	
+
 	public IndicativeAdapter(String title, String choosenIndicative, Context context) {
 		this.choosenIndicative = choosenIndicative;
 		this.title = title;
 		this.context = context;
-		this.inflater = LayoutInflater.from(context);
-	}
-	
-	@Override
-	public int getCount() {
-		return 28;
+		this.layoutInflater = LayoutInflater.from(context);
 	}
 
 	@Override
-	public HashMap<String, String> getItem(int position) {
+	public int getCount() {
+		return amountMenuItens;
+	}
+
+	@Override
+	public HashMap<String, String> getItem(int positionOneMenuState) {
 		HashMap<String, String> state = null;
 		try {
-			state = StateController.getInstance(context).readState(position);
+			state = StateController.getInstance(context).readState(positionOneMenuState);
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
+
 		return state;
 	}
 
 	@Override
-	public long getItemId(int position) {
-		return position;
+	public long getItemId(int positionOneMenuState) {
+		return positionOneMenuState;
 	}
-	
+
 	@Override
-	public View getView(int position, View view, ViewGroup viewGroup) {
+	public View getView(int positionOneMenuState, View view, ViewGroup viewGroup) {
 		if(view == null) {
-			view = this.inflater.inflate(R.layout.listview_item_indicativos, null);
-			holder = new ViewHolder();
-				
-			holder.tvName = (TextView) view.findViewById(R.id.textview_lista_indicativos_nome);
-			holder.tvIndicativeValue = (TextView) view.findViewById
+			view = this.layoutInflater.inflate(R.layout.listview_item_indicativos, null);
+			viewHolder = new ViewHolder();
+
+			viewHolder.textViewName = (TextView) view.findViewById
+				(R.id.textview_lista_indicativos_nome);
+			viewHolder.textViewIndicativeValue = (TextView) view.findViewById
 				(R.id.textview_lista_indicativos_conteudo);
-			holder.tvFlags = (ImageView) view.findViewById
+			viewHolder.textViewFlags = (ImageView) view.findViewById
 				(R.id.imageview_lista_indicativos_bandeiras);
-			view.setTag(holder);
-		} 
-		else {
-			holder = (ViewHolder) view.getTag();
+
+			view.setTag(viewHolder);
 		}
-		
-		if(position == 0){	// Title.
-			holder.tvName.setText(title);
-			holder.tvName.setGravity(Gravity.CENTER_HORIZONTAL);
-			holder.tvIndicativeValue.setVisibility(View.GONE);
-			holder.tvFlags.setImageResource(NO_SELECTION);
-		} 
+		else {
+			viewHolder = (ViewHolder) view.getTag();
+		}
+
+		if(positionOneMenuState == 0) {	// Title.
+			viewHolder.textViewName.setText(title);
+			viewHolder.textViewName.setGravity(Gravity.CENTER_HORIZONTAL);
+			viewHolder.textViewIndicativeValue.setVisibility(View.GONE);
+			viewHolder.textViewFlags.setImageResource(NO_SELECTION);
+		}
 		else {	// Indicative and it's values.
-			state = getItem(position-1);
-			holder.tvName.setText(state.get("nome"));
-			holder.tvIndicativeValue.setText(getValue(state.get(choosenIndicative)) );
-			holder.tvIndicativeValue.setVisibility(View.VISIBLE);
-			
-			int idFlag = context.getResources().getIdentifier(flags[position-1], "drawable", 
-				context.getPackageName());
-			holder.tvFlags.setImageResource(idFlag);
+			state = getItem(positionOneMenuState - titlePosition);
+
+			viewHolder.textViewName.setText(state.get("nome"));
+			viewHolder.textViewIndicativeValue.setText(getValue(state.get(choosenIndicative)));
+			viewHolder.textViewIndicativeValue.setVisibility(View.VISIBLE);
+
+			int idFlag = 0;
+			idFlag = context.getResources().getIdentifier(flagsNames[positionOneMenuState - 
+				titlePosition], "drawable", context.getPackageName());
+
+			viewHolder.textViewFlags.setImageResource(idFlag);
 		}
 
 		return view;
 	}
-	
+
 	// Split the line whe it contains an ":".
 	private String getValue(String line) {
 		if(line.contains(":")) {
 			String parts[] = line.split(": ");
 			return parts[1];
 		}
-		
+		else {
+			// Do nothing.
+		}
+
 		return line;
 	}
 }
