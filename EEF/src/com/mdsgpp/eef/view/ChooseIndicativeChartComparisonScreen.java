a5ead7f3d7b388e8d47cf9335fbae5673d3f9223
Makrau <iago006@hotmail.com>
2015-05-12 10:51:16 -0300

    English Translation: view/ChooseIndicativeChartComparisonScreen

--- EEF/src/com/mdsgpp/eef/view/ChooseIndicativeChartComparisonScreen.java
+++ EEF/src/com/mdsgpp/eef/view/ChooseIndicativeChartComparisonScreen.java
@@ -18,192 +18,192 @@ public class ChooseIndicativeChartComparisonScreen extends ChoosesIndicative {
 	private int position1;
 	private int position2;
 	
-	private boolean censusRadioVisibility;
-	private boolean ClassStudentsRadioVisibility;
-	private boolean bHorasAula;
-	private boolean bTaxaDistorcao;
-	private boolean bTaxaAbandono;
-	private boolean bAprovacao;
-	private boolean bIdeb;
-	private boolean bPib;
-	private boolean bPopulacao;
-	private boolean bPrimeirosProjetos;
-	private boolean bProjetosCnpq;
-	private boolean bProjetosDifusao;
-	private boolean bProjetosIniciacao;
-	private boolean bProjetosJovens;
+	private boolean radioButtonVisibilityCensus;
+	private boolean radioButtonVisibilityClassSize;
+	private boolean radioButtonVisibilityClassHours;
+	private boolean radioButtonVisibilityDistortionRate;
+	private boolean radioButtonVisibilityDropoutRate;
+	private boolean radioButtonVisibilityAprovation;
+	private boolean radioButtonVisibilityIdeb;
+	private boolean radioButtonVisibilityPIB;
+	private boolean radioButtonVisibilityPopulation;
+	private boolean radioButtonVisibilityFirstProjects;
+	private boolean radioButtonVisibilityCnpqProjects;
+	private boolean radioButtonVisibilityDifusionProjects;
+	private boolean radioButtonVisibilityInitiationProjects;
+	private boolean radioButtonVisibilityYoungProjects;
 
-	private RadioButton rbIdebIniciais;
-	private RadioButton rbIdebFinais;
-	private RadioButton rbIdebMedio;
-	private RadioButton rbPib;
-	private RadioButton rbPopulacao;
-	private RadioButton rbPrimeirosProjetosQuantidade;
-	private RadioButton rbPrimeirosProjetosValores;
-	private RadioButton rbProjetosCnpqQuantidade;
-	private RadioButton rbProjetosCnpqValores;
-	private RadioButton rbProjetosDifusaoQuantidade; 
-	private RadioButton rbProjetosDifusaoValores;
-	private RadioButton rbProjetosIniciacaoQuantidade;
-	private RadioButton rbProjetosIniciacaoValores; 
-	private RadioButton rbProjetosJovensQuantidade;
-	private RadioButton rbProjetosJovensValores;
-	private RadioButton rClassStudentsRadioVisibilityFundamental;
-	private RadioButton rClassStudentsRadioVisibilityMedio;
-	private RadioButton rbHorasAulaFundamental;
-	private RadioButton rbHorasAulaMedio;
-	private RadioButton rbTaxaDeDistorcaoFundamental;
-	private RadioButton rbTaxaDeDistorcaoMedio;
-	private RadioButton rbTaxaDeAprocaoFundamental;
-	private RadioButton rbTaxaDeAprovacaoMedio;
-	private RadioButton rbTaxaDeAbandonoFundamental;
-	private RadioButton rbTaxaDeAbandonoMedio;
-	private RadioButton rcensusRadioVisibilityAnosIniciaisFundamental;
-	private RadioButton rcensusRadioVisibilityAnosFinaisFundamental;
-	private RadioButton rcensusRadioVisibilityEsninoMedio;
-	private RadioButton rcensusRadioVisibilityEJAFundamental;
-	private RadioButton rcensusRadioVisibilityEJAMedio;
+	private RadioButton radioButtonEarlyYearsIdeb;
+	private RadioButton radioButtonFinalYearsIdeb;
+	private RadioButton radioButtonHighSchoolIdeb;
+	private RadioButton radioButtonPIB;
+	private RadioButton radioButtonPopulation;
+	private RadioButton radioButtonFirstProjectsQuantity;
+	private RadioButton radioButtonFirstProjectsValues;
+	private RadioButton radioButtonCnpqProjectsQuantity;
+	private RadioButton radioButtonCnpqProjectsValues;
+	private RadioButton radioButtonDifusionProjectsQuantity; 
+	private RadioButton radioButtonDifusionProjectsValues;
+	private RadioButton radioButtonInitiationProjectsQuantity;
+	private RadioButton radioButtonInitiationProjectsValues; 
+	private RadioButton radioButtonYoungProjectsQuantity;
+	private RadioButton radioButtonYoungProjectsValues;
+	private RadioButton radioButtonClassAverageSizeElementarySchool;
+	private RadioButton radioButtonClassAverageSizeHighSchool;
+	private RadioButton radioButtonClassHoursElementarySchool;
+	private RadioButton radioButtonClassHoursHighSchool;
+	private RadioButton radioButtonDistortionRateElementarySchool;
+	private RadioButton radioButtonDistortionRateHighSchool;
+	private RadioButton radioButtonUtilizationRateElementarySchool;
+	private RadioButton radioButtonUtilizationRateHighSchool;
+	private RadioButton radioButtonDropoutRateElementarySchool;
+	private RadioButton radioButtonDropoutRateHighSchool;
+	private RadioButton radioButtonEarlyYearsElementarySchoolCensus;
+	private RadioButton radioButtonFinalYearsElementarySchoolCensus;
+	private RadioButton radioButtonHighSchoolCensus;
+	private RadioButton radioButtonEJAElementarySchoolCensus;
+	private RadioButton radioButtonEJAHighSchoolCensus;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela_escolhe_indicativo_grafico);
 		
-		inicializaRadioButtons();
-		capturaInformacoes();
-		escondeRadioButtons();
+		radioButtonsStart();
+		catchInformation();
+		hideRadionButton();
 	}
 	
 	// Get information about the compared states.
-	public void capturaInformacoes() {
-		Intent intentRecebida = getIntent();
+	public void catchInformation() {
+		Intent receivedIntent = getIntent();
 		
-		position1 = intentRecebida.getIntExtra("INDEX_ESTADO1_ESCOLHIDO", 0);
-		position2 = intentRecebida.getIntExtra("INDEX_ESTADO2_ESCOLHIDO", 0);
+		position1 = receivedIntent.getIntExtra("INDEX_ESTADO1_ESCOLHIDO", 0);
+		position2 = receivedIntent.getIntExtra("INDEX_ESTADO2_ESCOLHIDO", 0);
 
-		bIdeb = intentRecebida.getBooleanExtra("CB_IDEB", false);
-		bPib = intentRecebida.getBooleanExtra("CB_PIB", false);
-		bPopulacao = intentRecebida.getBooleanExtra("CB_POPULACAO", false);
-		bPrimeirosProjetos = intentRecebida.getBooleanExtra
+		radioButtonVisibilityIdeb = receivedIntent.getBooleanExtra("CB_IDEB", false);
+		radioButtonVisibilityPIB = receivedIntent.getBooleanExtra("CB_PIB", false);
+		radioButtonVisibilityPopulation = receivedIntent.getBooleanExtra("CB_POPULACAO", false);
+		radioButtonVisibilityFirstProjects = receivedIntent.getBooleanExtra
 			("CB_PRIMEIROS_PROJETOS", false);
-		bProjetosCnpq = intentRecebida.getBooleanExtra("CB_PROJETOS_CNPQ",
+		radioButtonVisibilityCnpqProjects = receivedIntent.getBooleanExtra("CB_PROJETOS_CNPQ",
 			false);
-		bProjetosDifusao = intentRecebida.getBooleanExtra("CB_PROJETOS_DIFUSAO",
+		radioButtonVisibilityDifusionProjects = receivedIntent.getBooleanExtra("CB_PROJETOS_DIFUSAO",
 			false);
-		bProjetosIniciacao = intentRecebida.getBooleanExtra
+		radioButtonVisibilityInitiationProjects = receivedIntent.getBooleanExtra
 			("CB_PROJETOS_INICIACAO", false);
-		bProjetosJovens = intentRecebida.getBooleanExtra("CB_PROJETOS_JOVENS",
+		radioButtonVisibilityYoungProjects = receivedIntent.getBooleanExtra("CB_PROJETOS_JOVENS",
 			false);
-		censusRadioVisibility = intentRecebida.getBooleanExtra("CB_CENSO", false);
-		ClassStudentsRadioVisibility = intentRecebida.getBooleanExtra("CB_ALUNOS_TURMA", false);
-		bHorasAula = intentRecebida.getBooleanExtra("CB_HORAS_AULA", false);
-		bTaxaDistorcao = intentRecebida.getBooleanExtra("CB_TAXA_DISTORCAO",
+		radioButtonVisibilityCensus = receivedIntent.getBooleanExtra("CB_CENSO", false);
+		radioButtonVisibilityClassSize = receivedIntent.getBooleanExtra("CB_ALUNOS_TURMA", false);
+		radioButtonVisibilityClassHours = receivedIntent.getBooleanExtra("CB_HORAS_AULA", false);
+		radioButtonVisibilityDistortionRate = receivedIntent.getBooleanExtra("CB_TAXA_DISTORCAO",
 			false);
-		bTaxaAbandono = intentRecebida.getBooleanExtra("CB_TAXA_ABANDONO",
+		radioButtonVisibilityDropoutRate = receivedIntent.getBooleanExtra("CB_TAXA_ABANDONO",
 			false);
-		bAprovacao = intentRecebida.getBooleanExtra("CB_APROVACAO", false);
+		radioButtonVisibilityAprovation = receivedIntent.getBooleanExtra("CB_APROVACAO", false);
 		
 	}
 	
 	// Start all radio buttons on screen.
-	public void inicializaRadioButtons() {
-		rbIdebIniciais = (RadioButton) findViewById
+	public void radioButtonsStart() {
+		radioButtonEarlyYearsIdeb = (RadioButton) findViewById
 			(R.id.radio_ideb_fundamental_iniciai);
-		rbIdebFinais = (RadioButton) findViewById
+		radioButtonFinalYearsIdeb = (RadioButton) findViewById
 			(R.id.radio_ideb_fundamental_finais);
-		rbIdebMedio = (RadioButton) findViewById(R.id.radio_ideb_medio);
-		rbPopulacao = (RadioButton) findViewById(R.id.radio_populacao);
-		rbPrimeirosProjetosQuantidade = (RadioButton) findViewById
+		radioButtonHighSchoolIdeb = (RadioButton) findViewById(R.id.radio_ideb_medio);
+		radioButtonPopulation = (RadioButton) findViewById(R.id.radio_populacao);
+		radioButtonFirstProjectsQuantity = (RadioButton) findViewById
 			(R.id.radio_primeiros_projetos_qtd);
-		rbPrimeirosProjetosValores = (RadioButton) findViewById
+		radioButtonFirstProjectsValues = (RadioButton) findViewById
 			(R.id.radio_primeiros_projetos_investimento);
-		rbProjetosCnpqQuantidade = (RadioButton) findViewById
+		radioButtonCnpqProjectsQuantity = (RadioButton) findViewById
 			(R.id.radio_apoio_cnpq_qtd);
-		rbProjetosCnpqValores = (RadioButton) findViewById
+		radioButtonCnpqProjectsValues = (RadioButton) findViewById
 			(R.id.radio_apoio_cnpq_investimento);
-		rbProjetosDifusaoQuantidade = (RadioButton) findViewById
+		radioButtonDifusionProjectsQuantity = (RadioButton) findViewById
 			(R.id.radio_difusao_tecnologica_qtd);
-		rbProjetosDifusaoValores = (RadioButton) findViewById
+		radioButtonDifusionProjectsValues = (RadioButton) findViewById
 			(R.id.radio_difusao_tecnologica_investimento);
-		rbProjetosIniciacaoQuantidade = (RadioButton) findViewById
+		radioButtonInitiationProjectsQuantity = (RadioButton) findViewById
 			(R.id.radio_projetos_inct_qtd);
-		rbProjetosIniciacaoValores = (RadioButton) findViewById
+		radioButtonInitiationProjectsValues = (RadioButton) findViewById
 			(R.id.radio_projetos_inct_investimento);
-		rbProjetosJovensQuantidade = (RadioButton) findViewById
+		radioButtonYoungProjectsQuantity = (RadioButton) findViewById
 			(R.id.radio_jovens_pesquisadores_qtd);
-		rbProjetosJovensValores = (RadioButton) findViewById
+		radioButtonYoungProjectsValues = (RadioButton) findViewById
 			(R.id.radio_jovens_pesquisadores_investimento);
-		rClassStudentsRadioVisibilityFundamental = (RadioButton) findViewById
+		radioButtonClassAverageSizeElementarySchool = (RadioButton) findViewById
 			(R.id.radio_alunos_por_turma_fundamental);
-		rClassStudentsRadioVisibilityMedio = (RadioButton) findViewById
+		radioButtonClassAverageSizeHighSchool = (RadioButton) findViewById
 			(R.id.radio_alunos_por_turma_medio);
-		rbHorasAulaFundamental = (RadioButton) findViewById
+		radioButtonClassHoursElementarySchool = (RadioButton) findViewById
 			(R.id.radio_horas_aula_fundamental);
-		rbHorasAulaMedio = (RadioButton) findViewById
+		radioButtonClassHoursHighSchool = (RadioButton) findViewById
 			(R.id.radio_horas_aula_medio);
-		rbTaxaDeDistorcaoFundamental = (RadioButton) findViewById
+		radioButtonDistortionRateElementarySchool = (RadioButton) findViewById
 			(R.id.radio_taxa_distorcao_fundamental);
-		rbTaxaDeDistorcaoMedio = (RadioButton) findViewById
+		radioButtonDistortionRateHighSchool = (RadioButton) findViewById
 			(R.id.radio_taxa_distorcao_medio);
-		rbTaxaDeAprocaoFundamental = (RadioButton) findViewById
+		radioButtonUtilizationRateElementarySchool = (RadioButton) findViewById
 			(R.id.radio_taxa_aprovacao_fundamental);
-		rbTaxaDeAprovacaoMedio = (RadioButton) findViewById
+		radioButtonUtilizationRateHighSchool = (RadioButton) findViewById
 			(R.id.radio_taxa_aprovacao_medio);
-		rbTaxaDeAbandonoFundamental = (RadioButton) findViewById
+		radioButtonDropoutRateElementarySchool = (RadioButton) findViewById
 			(R.id.radio_taxa_abandono_fundamental);
-		rbTaxaDeAbandonoMedio = (RadioButton) findViewById
+		radioButtonDropoutRateHighSchool = (RadioButton) findViewById
 			(R.id.radio_taxa_abandono_medio);
-		rcensusRadioVisibilityAnosIniciaisFundamental = (RadioButton) findViewById
+		radioButtonEarlyYearsElementarySchoolCensus = (RadioButton) findViewById
 			(R.id.radio_censo_iniciais_fundamental);
-		rcensusRadioVisibilityAnosFinaisFundamental = (RadioButton) findViewById
+		radioButtonFinalYearsElementarySchoolCensus = (RadioButton) findViewById
 			(R.id.radio_censo_finais_fundamental);
-		rcensusRadioVisibilityEsninoMedio = (RadioButton) findViewById
+		radioButtonHighSchoolCensus = (RadioButton) findViewById
 			(R.id.radio_censo_ensino_medio);
-		rcensusRadioVisibilityEJAFundamental = (RadioButton) findViewById
+		radioButtonEJAElementarySchoolCensus = (RadioButton) findViewById
 			(R.id.radio_censo_eja_fundamental);
-		rcensusRadioVisibilityEJAMedio = (RadioButton) findViewById
+		radioButtonEJAHighSchoolCensus = (RadioButton) findViewById
 			(R.id.radio_censo_eja_medio);
 		
 	}
 	
 	// Allow the buttons to be viewed or not.
-	public void setVisibility(RadioButton radioButton, boolean visibilidade) {
-		int valorVisibilidade = (visibilidade) ? View.VISIBLE : View.GONE;
-		radioButton.setVisibility(valorVisibilidade);
+	public void setVisibility(RadioButton radioButton, boolean visibility) {
+		int visibilityValue = (visibility) ? View.VISIBLE : View.GONE;
+		radioButton.setVisibility(visibilityValue);
 	}
 	
 	// Hide radio buttons when the information is not available.
-	public void escondeRadioButtons() {
-		setVisibility(rbIdebIniciais, bIdeb);
-		setVisibility(rbIdebFinais, bIdeb);
-		setVisibility(rbIdebMedio, bIdeb);
-		setVisibility(rbPib, bPib);
-		setVisibility(rbPopulacao, bPopulacao);
-		setVisibility(rbPrimeirosProjetosQuantidade, bPrimeirosProjetos);
-		setVisibility(rbPrimeirosProjetosValores, bPrimeirosProjetos);
-		setVisibility(rbProjetosCnpqQuantidade, bProjetosCnpq);
-		setVisibility(rbProjetosCnpqValores, bProjetosCnpq);
-		setVisibility(rbProjetosDifusaoQuantidade, bProjetosDifusao);
-		setVisibility(rbProjetosDifusaoValores, bProjetosDifusao);
-		setVisibility(rbProjetosIniciacaoQuantidade, bProjetosIniciacao);
-		setVisibility(rbProjetosIniciacaoValores, bProjetosIniciacao);
-		setVisibility(rbProjetosJovensQuantidade, bProjetosJovens);
-		setVisibility(rbProjetosJovensValores, bProjetosJovens);
-		setVisibility(rClassStudentsRadioVisibilityFundamental, ClassStudentsRadioVisibility);
-		setVisibility(rClassStudentsRadioVisibilityMedio, ClassStudentsRadioVisibility);
-		setVisibility(rbHorasAulaFundamental, bHorasAula);
-		setVisibility(rbHorasAulaMedio, bHorasAula);
-		setVisibility(rbTaxaDeDistorcaoFundamental, bTaxaDistorcao);
-		setVisibility(rbTaxaDeDistorcaoMedio, bTaxaDistorcao);
-		setVisibility(rbTaxaDeAprocaoFundamental, bAprovacao);
-		setVisibility(rbTaxaDeAprovacaoMedio, bAprovacao);
-		setVisibility(rbTaxaDeAbandonoFundamental, bTaxaAbandono);
-		setVisibility(rbTaxaDeAbandonoMedio, bTaxaAbandono);
-		setVisibility(rcensusRadioVisibilityAnosIniciaisFundamental, censusRadioVisibility);
-		setVisibility(rcensusRadioVisibilityAnosFinaisFundamental, censusRadioVisibility);
-		setVisibility(rcensusRadioVisibilityEsninoMedio, censusRadioVisibility);
-		setVisibility(rcensusRadioVisibilityEJAFundamental, censusRadioVisibility);
-		setVisibility(rcensusRadioVisibilityEJAMedio, censusRadioVisibility);
+	public void hideRadionButton() {
+		setVisibility(radioButtonEarlyYearsIdeb, radioButtonVisibilityIdeb);
+		setVisibility(radioButtonFinalYearsIdeb, radioButtonVisibilityIdeb);
+		setVisibility(radioButtonHighSchoolIdeb, radioButtonVisibilityIdeb);
+		setVisibility(radioButtonPIB, radioButtonVisibilityPIB);
+		setVisibility(radioButtonPopulation, radioButtonVisibilityPopulation);
+		setVisibility(radioButtonFirstProjectsQuantity, radioButtonVisibilityFirstProjects);
+		setVisibility(radioButtonFirstProjectsValues, radioButtonVisibilityFirstProjects);
+		setVisibility(radioButtonCnpqProjectsQuantity, radioButtonVisibilityCnpqProjects);
+		setVisibility(radioButtonCnpqProjectsValues, radioButtonVisibilityCnpqProjects);
+		setVisibility(radioButtonDifusionProjectsQuantity, radioButtonVisibilityDifusionProjects);
+		setVisibility(radioButtonDifusionProjectsValues, radioButtonVisibilityDifusionProjects);
+		setVisibility(radioButtonInitiationProjectsQuantity, radioButtonVisibilityInitiationProjects);
+		setVisibility(radioButtonInitiationProjectsValues, radioButtonVisibilityInitiationProjects);
+		setVisibility(radioButtonYoungProjectsQuantity, radioButtonVisibilityYoungProjects);
+		setVisibility(radioButtonYoungProjectsValues, radioButtonVisibilityYoungProjects);
+		setVisibility(radioButtonClassAverageSizeElementarySchool, radioButtonVisibilityClassSize);
+		setVisibility(radioButtonClassAverageSizeHighSchool, radioButtonVisibilityClassSize);
+		setVisibility(radioButtonClassHoursElementarySchool, radioButtonVisibilityClassHours);
+		setVisibility(radioButtonClassHoursHighSchool, radioButtonVisibilityClassHours);
+		setVisibility(radioButtonDistortionRateElementarySchool, radioButtonVisibilityDistortionRate);
+		setVisibility(radioButtonDistortionRateHighSchool, radioButtonVisibilityDistortionRate);
+		setVisibility(radioButtonUtilizationRateElementarySchool, radioButtonVisibilityAprovation);
+		setVisibility(radioButtonUtilizationRateHighSchool, radioButtonVisibilityAprovation);
+		setVisibility(radioButtonDropoutRateElementarySchool, radioButtonVisibilityDropoutRate);
+		setVisibility(radioButtonDropoutRateHighSchool, radioButtonVisibilityDropoutRate);
+		setVisibility(radioButtonEarlyYearsElementarySchoolCensus, radioButtonVisibilityCensus);
+		setVisibility(radioButtonFinalYearsElementarySchoolCensus, radioButtonVisibilityCensus);
+		setVisibility(radioButtonHighSchoolCensus, radioButtonVisibilityCensus);
+		setVisibility(radioButtonEJAElementarySchoolCensus, radioButtonVisibilityCensus);
+		setVisibility(radioButtonEJAHighSchoolCensus, radioButtonVisibilityCensus);
 	}
 	
 	// Implements the action on button next.
