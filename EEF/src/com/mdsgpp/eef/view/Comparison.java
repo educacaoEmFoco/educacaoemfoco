e3342765928fdfc7fd91b37f79f5ad9b88cdc94d
Jonathan Rufino <jonathan_rufinotkd@hotmail.com>
2015-05-18 20:59:59 -0300

    Log implemented: view/Comparison

--- EEF/src/com/mdsgpp/eef/view/Comparison.java
+++ EEF/src/com/mdsgpp/eef/view/Comparison.java
@@ -56,6 +56,8 @@ public abstract class Comparison extends Activity {
 	* Here we initialize them with default values.
 	*/
 	private void initializesSpinners() {
+		Debug.log("Comparison - initializesSpinners()", "initializing spinners", Debug.INFO);
+
 		states01 = new ArrayList<String>();
 		states02 = new ArrayList<String>();
 		allStates = new ArrayList<String>();
@@ -85,15 +87,21 @@ public abstract class Comparison extends Activity {
 				// Nothing to do.
 			}
 		});
+
+		Debug.log("Comparison - initializesSpinners()", "spinners initialized!", Debug.INFO);
 	}
 
 	private void setAdapterSpinner01() {
+		Debug.log("Comparison - setAdapterSpinner01()", "setting values to Spinner01", Debug.DEBUG);
+
 		statesAdapter01 = new ArrayAdapter<String>(this, R.layout.spinner_item, states01);
 		statesAdapter01.setDropDownViewResource(android.R.layout.simple_list_item_1);
 		statesSpinner01.setAdapter(statesAdapter01);
 	}
 
 	private void setAdapterSpinner02() {
+		Debug.log("Comparison - setAdapterSpinner02()", "setting values to Spinner02", Debug.DEBUG);
+
 		statesAdapter02 = new ArrayAdapter<String>(this, R.layout.spinner_item, states02);
 		statesAdapter02.setDropDownViewResource(android.R.layout.simple_list_item_1);
 
@@ -103,33 +111,36 @@ public abstract class Comparison extends Activity {
 
 	// Fill the array states with the name of all brazilian states.
 	private ArrayList<String> fillStates(ArrayList<String> states) {
+		Debug.log("Comparison - fillStates()", "filling the arraylist with the state names", 
+			Debug.DEBUG);
+
 		states.clear();
 
 		states.add("Acre");
 		states.add("Alagoas");
-		states.add("Amap�");
+		states.add("Amapá");
 		states.add("Amazonas");
 		states.add("Bahia");
-		states.add("Cear�");
+		states.add("Ceará");
 		states.add("Distrito Federal");
-		states.add("Esp�rito Santo");
-		states.add("Goi�s");
-		states.add("Maranh�o");
+		states.add("Espírito Santo");
+		states.add("Goiás");
+		states.add("Maranhão");
 		states.add("Mato Grosso");
 		states.add("Mato Grosso do Sul");
 		states.add("Minas Gerais");
-		states.add("Par�");
-		states.add("Para�ba");
-		states.add("Paran�");
+		states.add("Pará");
+		states.add("Paraíba");
+		states.add("Paraná");
 		states.add("Pernambuco");
-		states.add("Piau�");
+		states.add("Piauí");
 		states.add("Rio de Janeiro");
 		states.add("Rio Grande do Norte");
 		states.add("Rio Grande do Sul");
-		states.add("Rond�nia");
+		states.add("Rondônia");
 		states.add("Roraima");
 		states.add("Santa Catarina");
-		states.add("S�o Paulo");
+		states.add("São Paulo");
 		states.add("Sergipe");
 		states.add("Tocantins");
 		
@@ -138,6 +149,9 @@ public abstract class Comparison extends Activity {
 
 	// Refresh the first spinner with new values.
 	private void refreshesSpinner01Values(String name) {
+		Debug.log("Comparison - refreshesSpinner01Values()", "refresing spinner01 values", 
+			Debug.DEBUG);
+
 		String selectedState = statesSpinner01.getSelectedItem().toString();
 		states01 = fillStates(states01);
 		states01.remove(name);
@@ -147,6 +161,9 @@ public abstract class Comparison extends Activity {
 
 	// Refresh the second spinner with new values.
 	private void refreshesSpinner02Values(String name) {
+		Debug.log("Comparison - refreshesSpinner02Values()", "refresing spinner02 values", 
+			Debug.DEBUG);
+
 		String selectedState = statesSpinner.getSelectedItem().toString();
 		states02 = fillStates(states02);
 		states02.remove(name);
@@ -159,7 +176,14 @@ public abstract class Comparison extends Activity {
 	
 	// Implements the action of button to start comparison.
 	public void clickButtonAboutComparison(View view) {
+		Debug.log("Comparison - clickButtonAboutComparison", 
+			"click detected in About Comparison button", Debug.INFO);
+
 		Intent intent = new Intent(this, ConsultationAboutScreen.class);
+
+		Debug.log("Comparison - clickButtonAboutComparison", 
+			"starting new activity: About Comparison", Debug.DEBUG);
+
 		startActivity(intent);
 	}	
 	
