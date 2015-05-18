6c839fdc92e3fcd34be21452f524f82d8d507d4b
Makrau <iago006@hotmail.com>
2015-05-17 21:10:38 -0300

    Log implemented: view/MainActivity

--- EEF/src/com/mdsgpp/eef/view/MainActivity.java
+++ EEF/src/com/mdsgpp/eef/view/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mdsgpp.eef.view;
 
 import com.mdsgpp.eef.R;
+import com.mdsgpp.eef.debug.Debug;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -28,6 +29,8 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {    	
 		switch(item.getItemId()) {
     		case R.id.sobre:
+    			Debug.log("MainActivity - onOptionsItemSelected()", "Menu item clicked detected", 
+    				Debug.DEBUG);
     			openAboutScreen();
     			break;
     		default:
@@ -39,30 +42,40 @@ public class MainActivity extends Activity {
     
     // Open the screen about.
     public void openAboutScreen() {
+    	Debug.log("MainActivity - openAboutScreen()", "opening AboutScreen", Debug.DEBUG);
+    	
     	Intent intent = new Intent(this, AboutScreen.class);
     	startActivity(intent);
     }
 
     // Implements the action on button general comparison.
     public void clickGeneralComparisonButton(View view) {
+    	Debug.log("MainActivity - clickGeneralComparisonButton()",
+    		"click detected in General Comparison Button!", Debug.INFO);
     	Intent intent = new Intent(this, GeneralComparisonsScreen.class);
 		startActivity(intent);
     }
     
     // Implements the action on button general consultation.
     public void clickGeneralConsultButton(View view) {
+    	Debug.log("MainActivity - clickGeneralConsultButton()",
+        		"click detected in General Consult Button!", Debug.INFO);
     	Intent intent = new Intent(this, StateListScreen.class);
     	startActivity(intent);
     }
     
     // Implements the action on button feed.
     public void clickFeedButton(View view) {
+    	Debug.log("MainActivity - clickFeedButton()",
+        		"click detected in Feed Button!", Debug.INFO);
     	Intent intent = new Intent(this, FeedScreen.class);
     	startActivity(intent);
     }
 
     // Implements the action on button consultation by indicative.
     public void clickConsultByIndicativeButton(View view) {
+    	Debug.log("MainActivity - clickConsultByIndicativeButton()",
+        		"click detected in Consult by Indicative Button!", Debug.INFO);
     	Intent intent = new Intent(this, ConsultationByIndicativeScreen.class);
     	startActivity(intent);
     }
