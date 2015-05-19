968341653821b828b430c745216efd0fa27ed5f8
Jonathan Rufino <jonathan_rufinotkd@hotmail.com>
2015-05-18 22:35:35 -0300

    Log implemented: view/GeneralComparisonsScreen

--- EEF/src/com/mdsgpp/eef/view/GeneralComparisonsScreen.java
+++ EEF/src/com/mdsgpp/eef/view/GeneralComparisonsScreen.java
@@ -1,6 +1,13 @@
+/*******************************************************
+ * File: GeneralComparisonsScreen.java
+ *
+ * Purpose: Handle what type of comparison will be done.
+ *******************************************************/
+
 package com.mdsgpp.eef.view;
 
 import com.mdsgpp.eef.R;
+import com.mdsgpp.eef.debug.Debug;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -18,19 +25,32 @@ public class GeneralComparisonsScreen extends Activity {
 		setContentView(R.layout.activity_tela_comparacoes_gerais);
 	}
 
+	// If the actionbar is present adds options buttons to it.
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+		Debug.log("GeneralComparisonsScreen - onCreateOptionsMenu", 
+			"adding buttons to the action bar", Debug.INFO);
+
 		getMenuInflater().inflate(R.menu.menu_telas, menu);
+
 		return true;
 	}
 	
-    public boolean onOptionsItemSelected(MenuItem item) {    	
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	Debug.log("GeneralComparisonsScreen - onOptionsItemSelected", "item selected in action bar",
+    		Debug.INFO);
+
 		switch(item.getItemId()) {
 			case R.id.sobre:
+				Debug.log("GeneralComparisonsScreen - onOptionsItemSelected", 
+					"About Button clicked, openning about screen", Debug.INFO);
+
 				opensAboutScreen();
 				break;
 			case android.R.id.home:
+				Debug.log("GeneralComparisonsScreen - onOptionsItemSelected",
+					"Home Button clicked, going back to previous activity", Debug.DEBUG);
+
 		        NavUtils.navigateUpFromSameTask(this);
 		        break;
 			default:
@@ -43,18 +63,27 @@ public class GeneralComparisonsScreen extends Activity {
     
     // Open screen about.
     public void opensAboutScreen() {
+    	Debug.log("GeneralComparisonsScreen - opensAboutScreen", "openning about screen", 
+    		Debug.DEBUG);
+
     	Intent intent = new Intent(this, GeneralComparisonAboutScreen.class);
     	startActivity(intent);
     }
 	
 	// Implements action on button compararison.
 	public void clickButtonComparison(View view) {
+		Debug.log("GeneralComparisonsScreen - clickButtonComparison", "openning comparison screen", 
+    		Debug.DEBUG);
+
 		Intent intent = new Intent(this, ComparisonScreen.class);
 		startActivity(intent);
 	}
 	
 	// Implements action on button indicatives.
 	public void clickButtonIndicatives(View view) {
+		Debug.log("GeneralComparisonsScreen - clickButtonIndicatives", "openning indicative screen", 
+    		Debug.DEBUG);
+
 		Intent intent = new Intent(this, ConsultationByIndicativeResultScreen.class);
 		startActivity(intent);
 	}	
